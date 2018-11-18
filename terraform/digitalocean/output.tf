82823fdb8170ca554c3be2a2ea7a53be9a21f4e5
Marko Mudrinić <mudrinic.mare@gmail.com>
2018-11-18 19:12:21 +0100

    Add an example Terraform script for DigitalOcean

---
+++ terraform/digitalocean/output.tf
@@ -0,0 +1,20 @@
+output "kubeone_api" {
+  value = {
+    endpoint = "${digitalocean_loadbalancer.control_plane.ip}"
+  }
+}
+
+output "kubeone_hosts" {
+  value = {
+    control_plane = {
+      public_address  = "${digitalocean_droplet.control_plane.*.ipv4_address}"
+      private_address = "${digitalocean_droplet.control_plane.*.ipv4_address_private}"
+      hostnames       = "${digitalocean_droplet.control_plane.*.name}"
+      ssh_user        = "root"
+      ssh_port        = "${var.ssh_port}"
+
+      ssh_agent_socket     = "${var.ssh_agent_socket}"
+      ssh_private_key_file = "${var.ssh_private_key_file}"
+    }
+  }
+}
