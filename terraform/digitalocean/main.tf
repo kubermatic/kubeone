e6e752a79a409b976e84837bfde0d86207d61b81
Marko Mudrinić <mudrinic.mare@gmail.com>
2018-11-19 12:22:36 +0100

    Add an example Terraform script for DigitalOcean (#32)

---
+++ terraform/digitalocean/main.tf
@@ -0,0 +1,55 @@
+provider "digitalocean" {}
+
+locals {
+    kube_cluster_tag = "kubernetes-cluster:${var.cluster_name}"
+}
+
+resource "digitalocean_tag" "kube_cluster_tag" {
+    name = "${local.kube_cluster_tag}"
+}
+
+resource "digitalocean_ssh_key" "deployer" {
+    name = "${var.cluster_name}-deployer-key"
+    public_key = "${file("${var.ssh_public_key_file}")}"
+}
+
+resource "digitalocean_droplet" "control_plane" {
+    count = "${var.control_plane_count}"
+    name = "${var.cluster_name}-control-plane-${count.index + 1}"
+
+    tags = [
+        "${local.kube_cluster_tag}"
+    ]
+
+    image = "${var.droplet_image}"
+    region = "${var.region}"
+    size = "${var.droplet_size}"
+
+    private_networking = "${var.droplet_private_networking}"
+    monitoring = "${var.droplet_monitoring}"
+    ipv6 = "${var.droplet_ipv6}"
+
+    ssh_keys = [
+        "${digitalocean_ssh_key.deployer.id}"
+    ]
+}
+
+resource "digitalocean_loadbalancer" "control_plane" {
+    name = "${var.cluster_name}-load-balancer"
+    region = "${var.region}"
+
+    forwarding_rule {
+        entry_port = 6443
+        entry_protocol = "tcp"
+
+        target_port = 6443
+        target_protocol = "tcp"
+    }
+
+    healthcheck {
+        port = 6443
+        protocol = "tcp"
+    }
+
+    droplet_tag = "${local.kube_cluster_tag}"
+}
---
+++ terraform/digitalocean/variables.tf
@@ -0,0 +1,57 @@
+variable "cluster_name" {
+  description = "Name of the cluster"
+}
+
+variable "region" {
+  default     = "fra1"
+  description = "Region to speak to"
+}
+
+variable "control_plane_count" {
+  default     = 3
+  description = "Number of master instances"
+}
+
+variable "ssh_public_key_file" {
+  description = "SSH public key file"
+}
+
+variable "ssh_port" {
+  default = 22
+  description = "SSH port to be used to provision instances"
+}
+
+variable "ssh_private_key_file" {
+  description = "SSH private key file used to access instances"
+  default     = ""
+}
+
+variable "ssh_agent_socket" {
+  description = "SSH Agent socket, default to grab from $SSH_AUTH_SOCK"
+  default     = "env:SSH_AUTH_SOCK"
+}
+
+variable "droplet_image" {
+    default = "ubuntu-18-04-x64"
+    description = "Image to use for provisioning droplet"
+}
+
+variable "droplet_size" {
+    default = "s-2vcpu-4gb"
+    description = "Size of Droplets"
+}
+
+variable "droplet_private_networking" {
+  default = true
+  description = "Enable Private Networking on Droplets (recommended)"
+}
+
+variable "droplet_monitoring" {
+  default = false
+  description = "Enable advance Droplet metrics"
+}
+
+variable "droplet_ipv6" {
+  default = "false"
+  description = "Enable IPv6"
+}
