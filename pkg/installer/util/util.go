a80d89efa6f85130711a4b6f818764cdd99f5473
Alvaro Aleman <alv2412@googlemail.com>
2018-12-13 14:12:05 +0100

    Be strict when running shell commands

--- pkg/installer/util/util.go
+++ pkg/installer/util/util.go
@@ -63,7 +63,8 @@ type TemplateVariables map[string]interface{}
 
 // MakeShellCommand render text template with given `variables` render-context
 func MakeShellCommand(cmd string, variables TemplateVariables) (string, error) {
-	tpl, err := template.New("base").Parse(cmd)
+	commandWithSettings := fmt.Sprintf("set -euo pipefail;%s", cmd)
+	tpl, err := template.New("base").Parse(commandWithSettings)
 	if err != nil {
 		return "", fmt.Errorf("failed to parse shell script: %v", err)
 	}
