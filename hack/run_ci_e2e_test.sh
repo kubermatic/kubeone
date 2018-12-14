1936145a9dbddd6d880c7f0734db6202e57ccd97
p0lyn0mial <lukasz.szaszkiewicz@gmail.com>
2018-12-14 11:52:18 +0100

    adds more steps to run_ci_e2e_test script

--- hack/run_ci_e2e_test.sh
+++ hack/run_ci_e2e_test.sh
@@ -18,6 +18,23 @@ if ! [ -x "$(command -v terraform)" ]; then
  )
 fi
 
+# Generate SSH key pair
+if [ ! -f "$HOME/.ssh/id_rsa_kubeone_e2e" ]; then
+ echo "Generating SSH key pair"
+ ssh-keygen -f $HOME/.ssh/id_rsa_kubeone_e2e -N ''
+ SSH_PUBLIC_KEY_FILE="$HOME/.ssh/id_rsa_kubeone_e2e.pub"
+ SSH_PRIVATE_KEY_FILE="$HOME/.ssh/id_rsa_kubeone_e2e"
+ export SSH_PUBLIC_KEY_FILE
+ chmod 400 $SSH_PRIVATE_KEY_FILE
+ eval `ssh-agent`
+ ssh-add $SSH_PRIVATE_KEY_FILE
+fi
+
+# Build binaries
+echo "Building kubeone ..."
+make install
+
+
 # Start the tests
 echo "Runing E2E tests ..."
 go test -race -tags=e2e -v -timeout 30m  ./test/e2e/...
