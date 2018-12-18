928990285d8ce2651ce976803e6a757009aa8224
Guus van Weelden <guus@loodse.com>
2018-12-18 14:34:28 +0100

    update interface definition of archive

--- pkg/archive/archive.go
+++ pkg/archive/archive.go
@@ -2,6 +2,6 @@ package archive
 
 // Archive represents a file-backed collection of files.
 type Archive interface {
-	Add(file string, content string) error
+	Add(file string, content []byte) error
 	Close()
 }
