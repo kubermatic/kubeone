1c024b0c6f9515ae46b7252df971d6af9a5d3645
Christoph Mewes <christoph@loodse.com>
2018-12-18 14:29:58 +0100

    bring back explicit hostnames in clusters
    "kubeadm join" ignores hostnames in the init config and even if we wrote the kubeadm-flags.env with the explicit kubeadm alpha phase, the join command would override it again and get rid of it.

--- pkg/installer/version/kube112/05-master-join-cluster.go
+++ pkg/installer/version/kube112/05-master-join-cluster.go
@@ -14,18 +14,21 @@ func joinControlplaneNode(ctx *util.Context) error {
 func joinControlplaneNodeInternal(ctx *util.Context, node *config.HostConfig, conn ssh.Connection) error {
 	_, _, err := ctx.Runner.Run(`
 if [[ -f /etc/kubernetes/kubelet.conf ]]; then exit 0; fi
+
 if [[ -f /etc/systemd/system/kubelet.service.d/10-kubeadm.conf.disabled ]]; then
-	sudo mv /etc/systemd/system/kubelet.service.d/10-kubeadm.conf{.disabled,}
-	sudo systemctl daemon-reload
+  sudo mv /etc/systemd/system/kubelet.service.d/10-kubeadm.conf{.disabled,}
+  sudo systemctl daemon-reload
 fi
 
 sudo systemctl stop kubelet
 sudo {{ .JOIN_COMMAND }} \
-	 --experimental-control-plane \
-	 --ignore-preflight-errors=DirAvailable--etc-kubernetes-manifests
+	  --experimental-control-plane \
+	  --node-name="{{ .NODE_NAME }}" \
+     --ignore-preflight-errors=DirAvailable--etc-kubernetes-manifests
 `, util.TemplateVariables{
 		"WORK_DIR":     ctx.WorkDir,
 		"JOIN_COMMAND": ctx.JoinCommand,
+		"NODE_NAME":    node.Hostname,
 	})
 	return err
 }
