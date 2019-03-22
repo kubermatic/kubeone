879d210aaec5f38337c8b8cc391667504ffc3c13
Artiom Diomin <kron82@gmail.com>
2019-03-22 12:02:32 +0200

    Ensure machine-controller during install and upgrade
    Signed-off-by: Artiom Diomin <kron82@gmail.com>

--- pkg/installer/installation/certs.go
+++ pkg/installer/installation/certs.go
@@ -17,58 +17,10 @@ limitations under the License.
 package installation
 
 import (
-	"github.com/pkg/errors"
-
 	"github.com/kubermatic/kubeone/pkg/config"
 	"github.com/kubermatic/kubeone/pkg/ssh"
 	"github.com/kubermatic/kubeone/pkg/util"
 )
-
-func downloadCA(ctx *util.Context) error {
-	ctx.Logger.Infoln("Generating PKI…")
-
-	return ctx.RunTaskOnLeader(func(ctx *util.Context, _ *config.HostConfig, conn ssh.Connection) error {
-		ctx.Logger.Infoln("Running kubeadm…")
-
-		_, _, err := ctx.Runner.Run(`
-mkdir -p ./{{ .WORK_DIR }}/pki/etcd
-sudo cp /etc/kubernetes/pki/ca.crt ./{{ .WORK_DIR }}/pki/
-sudo cp /etc/kubernetes/pki/ca.key ./{{ .WORK_DIR }}/pki/
-sudo cp /etc/kubernetes/pki/sa.key ./{{ .WORK_DIR }}/pki/
-sudo cp /etc/kubernetes/pki/sa.pub ./{{ .WORK_DIR }}/pki/
-sudo cp /etc/kubernetes/pki/front-proxy-ca.crt ./{{ .WORK_DIR }}/pki/
-sudo cp /etc/kubernetes/pki/front-proxy-ca.key ./{{ .WORK_DIR }}/pki/
-sudo cp /etc/kubernetes/pki/etcd/ca.{crt,key} ./{{ .WORK_DIR }}/pki/etcd/
-
-sudo chown -R "$USER:$USER" ./{{ .WORK_DIR }}
-`, util.TemplateVariables{
-			"WORK_DIR": ctx.WorkDir,
-		})
-		if err != nil {
-			return err
-		}
-
-		ctx.Logger.Infoln("Downloading PKI files…")
-
-		err = ctx.Configuration.Download(conn, ctx.WorkDir+"/pki", "pki")
-		if err != nil {
-			return errors.Wrap(err, "failed to download PKI files")
-		}
-
-		if ctx.BackupFile != "" {
-			ctx.Logger.Infoln("Creating local backup…")
-
-			err = ctx.Configuration.Backup(ctx.BackupFile)
-			if err != nil {
-				// do not stop in case of failed backups, the user can
-				// always create the backup themselves if needed
-				ctx.Logger.Warnf("Failed to create backup: %v", err)
-			}
-		}
-
-		return nil
-	})
-}
 
 func deployCA(ctx *util.Context) error {
 	ctx.Logger.Infoln("Deploying PKI…")
--- pkg/installer/installation/worker.go
+++ pkg/installer/installation/worker.go
@@ -17,8 +17,6 @@ limitations under the License.
 package installation
 
 import (
-	"time"
-
 	"github.com/pkg/errors"
 
 	"github.com/kubermatic/kubeone/pkg/templates/machinecontroller"
@@ -30,18 +28,6 @@ func createWorkerMachines(ctx *util.Context) error {
 		return nil
 	}
 
-	ctx.Logger.Infoln("Waiting for machine-controller to come up…")
-	if err := machinecontroller.WaitForWebhook(ctx.DynamicClient); err != nil {
-		return errors.Wrap(err, "machine-controller-webhook did not come up")
-	}
-
-	if err := machinecontroller.WaitForMachineController(ctx.DynamicClient); err != nil {
-		return errors.Wrap(err, "machine-controller did not come up")
-	}
-
-	// it can still take a bit before the MC is actually ready
-	time.Sleep(10 * time.Second)
-
 	ctx.Logger.Infoln("Creating worker machines…")
-	return machinecontroller.DeployMachineDeployments(ctx)
+	return errors.Wrap(machinecontroller.DeployMachineDeployments(ctx), "failed to deploy Machines")
 }
