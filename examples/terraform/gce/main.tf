0d78cdf9073fb7c0666122472ab6beda901bb082
Christoph Mewes <github@xrstf.de>
2019-03-26 21:37:14 +0100

    Preliminary GCE example Terraform config (#307)
    * GCP terraform
    
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>
    
    * fix health check and add SSH support
    
    Signed-off-by: Christoph Mewes <christoph@loodse.com>
    
    * format gce tf config
    
    Signed-off-by: Artiom Diomin <kron82@gmail.com>

---
+++ examples/terraform/gce/main.tf
@@ -0,0 +1,153 @@
+/*
+Copyright 2019 The KubeOne Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+provider "google" {
+  region  = "${var.region}"
+  project = "${var.project}"
+}
+
+locals {
+  zones_count = "${length(data.google_compute_zones.available.names)}"
+}
+
+data "google_compute_zones" "available" {}
+
+data "google_compute_image" "control_plane_image" {
+  family  = "${var.control_plane_image_family}"
+  project = "${var.control_plane_image_project}"
+}
+
+resource "google_compute_network" "network" {
+  name                    = "${var.cluster_name}"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "${var.cluster_name}-subnet"
+  network       = "${google_compute_network.network.self_link}"
+  ip_cidr_range = "${var.cluster_network_cidr}"
+}
+
+resource "google_compute_firewall" "common" {
+  name    = "${var.cluster_name}-common"
+  network = "${google_compute_network.network.self_link}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["${var.ssh_port}"]
+  }
+
+  source_ranges = [
+    "0.0.0.0/0",
+  ]
+}
+
+resource "google_compute_firewall" "control_plane" {
+  name    = "${var.cluster_name}-control-plane"
+  network = "${google_compute_network.network.self_link}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["6443"]
+  }
+
+  source_ranges = [
+    "0.0.0.0/0",
+  ]
+}
+
+resource "google_compute_firewall" "internal" {
+  name    = "${var.cluster_name}-internal"
+  network = "${google_compute_network.network.self_link}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = [
+    "${var.cluster_network_cidr}",
+  ]
+}
+
+resource "google_compute_address" "lb_ip" {
+  name = "${var.cluster_name}-lb-ip"
+}
+
+resource "google_compute_http_health_check" "control_plane" {
+  name = "${var.cluster_name}-control-plane-health"
+
+  port         = 10256
+  request_path = "/healthz"
+
+  timeout_sec        = 3
+  check_interval_sec = 5
+}
+
+resource "google_compute_target_pool" "control_plane_pool" {
+  name = "${var.cluster_name}-control-plane"
+
+  instances = [
+    "${google_compute_instance.control_plane.*.self_link}",
+  ]
+
+  health_checks = [
+    "${google_compute_http_health_check.control_plane.self_link}",
+  ]
+}
+
+resource "google_compute_forwarding_rule" "control_plane" {
+  name       = "${var.cluster_name}-apiserver"
+  target     = "${google_compute_target_pool.control_plane_pool.self_link}"
+  port_range = "6443"
+  ip_address = "${google_compute_address.lb_ip.self_link}"
+}
+
+resource "google_compute_instance" "control_plane" {
+  count = "${var.control_plane_count}"
+
+  name         = "${var.cluster_name}-control-plane-${count.index+1}"
+  machine_type = "${var.control_plane_type}"
+  zone         = "${data.google_compute_zones.available.names[count.index % local.zones_count]}"
+
+  boot_disk {
+    initialize_params {
+      size  = "${var.control_plane_volume_size}"
+      image = "${data.google_compute_image.control_plane_image.self_link}"
+    }
+  }
+
+  network_interface {
+    subnetwork = "${google_compute_subnetwork.subnet.self_link}"
+  }
+
+  metadata = {
+    sshKeys = "${var.ssh_username}:${file(var.ssh_public_key_file)}"
+  }
+
+  service_account {
+    scopes = ["compute-rw", "storage-ro"]
+  }
+}
---
+++ examples/terraform/gce/variables.tf
@@ -0,0 +1,73 @@
+/*
+Copyright 2019 The KubeOne Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+variable "cluster_name" {
+  description = "prefix for cloud resources"
+}
+
+variable "project" {
+  description = "Project to be used for all resources"
+}
+
+variable "region" {
+  default     = "europe-west3"
+  description = "GCP region to speak to"
+}
+
+variable "control_plane_count" {
+  default     = 3
+  description = "Number of instances"
+}
+
+variable "control_plane_type" {
+  default     = "n1-standard-1"
+  description = "GCE instance type"
+}
+
+variable "control_plane_volume_size" {
+  default     = 100
+  description = "Size of the boot volume, in GB"
+}
+
+variable "control_plane_image_family" {
+  default     = "ubuntu-1804-lts"
+  description = "Image family to use for provisioning instances"
+}
+
+variable "control_plane_image_project" {
+  default     = "ubuntu-os-cloud"
+  description = "Project of the image to use for provisioning instances"
+}
+
+variable "cluster_network_cidr" {
+  default     = "10.240.0.0/24"
+  description = "Cluster network subnet cidr"
+}
+
+variable "ssh_port" {
+  description = "SSH port"
+  default     = 22
+}
+
+variable "ssh_public_key_file" {
+  default     = "~/.ssh/id_rsa.pub"
+  description = "SSH public key file"
+}
+
+variable "ssh_username" {
+  default     = "kubeadmin"
+  description = "Username to provision with the ssh_public_key_file"
+}
