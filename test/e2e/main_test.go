c264a20e9a227ef8177d3eb72731d453772e937a
Marko Mudrinić <mudrinic.mare@gmail.com>
2019-03-28 15:03:34 +0100

    Add additional checks to E2E tests
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- test/e2e/main_test.go
+++ test/e2e/main_test.go
@@ -19,8 +19,19 @@ limitations under the License.
 package e2e
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"strings"
 	"testing"
+	"time"
+
+	"github.com/Masterminds/semver"
+	"github.com/pkg/errors"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	dynclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // testRunIdentifier aka. the build number, a unique identifier for the test run.
@@ -51,4 +62,83 @@ func setupTearDown(p Provisioner, k Kubeone) func(t *testing.T) {
 			t.Errorf("%v", errProvisioner)
 		}
 	}
+}
+
+func waitForNodesReady(client dynclient.Client, expectedNumberOfNodes int) error {
+	return wait.Poll(5*time.Second, 5*time.Minute, func() (bool, error) {
+		nodes := corev1.NodeList{}
+		nodeListOpts := dynclient.ListOptions{}
+
+		err := client.List(context.Background(), &nodeListOpts, &nodes)
+		if err != nil {
+			return false, errors.Wrap(err, "unable to list nodes")
+		}
+
+		if len(nodes.Items) != expectedNumberOfNodes {
+			return false, nil
+		}
+
+		for _, n := range nodes.Items {
+			for _, c := range n.Status.Conditions {
+				if c.Type == corev1.NodeReady && c.Status != corev1.ConditionTrue {
+					return false, nil
+				}
+			}
+		}
+		return true, nil
+	})
+}
+
+func verifyVersion(client dynclient.Client, namespace string, targetVersion string) error {
+	reqVer, err := semver.NewVersion(targetVersion)
+	if err != nil {
+		return errors.Wrap(err, "desired version is invalid")
+	}
+
+	nodes := corev1.NodeList{}
+	nodeListOpts := dynclient.ListOptions{}
+	_ = nodeListOpts.SetLabelSelector(fmt.Sprintf("%s=%s", labelControlPlaneNode, ""))
+	err = client.List(context.Background(), &nodeListOpts, &nodes)
+	if err != nil {
+		return errors.Wrap(err, "failed to list nodes")
+	}
+
+	// Kubelet version check
+	for _, n := range nodes.Items {
+		kubeletVer, err := semver.NewVersion(n.Status.NodeInfo.KubeletVersion)
+		if err != nil {
+			return err
+		}
+		if reqVer.Compare(kubeletVer) != 0 {
+			return errors.Errorf("kubelet version mismatch: expected %v, got %v", reqVer.String(), kubeletVer.String())
+		}
+	}
+
+	apiserverPods := corev1.PodList{}
+	podsListOpts := dynclient.ListOptions{Namespace: namespace}
+	_ = podsListOpts.SetLabelSelector("component=kube-apiserver")
+	err = client.List(context.Background(), &podsListOpts, &apiserverPods)
+	if err != nil {
+		return errors.Wrap(err, "unable to list apiserver pods")
+	}
+
+	for _, p := range apiserverPods.Items {
+		apiserverVer, err := parseContainerImageVersion(p.Spec.Containers[0].Image)
+		if err != nil {
+			return errors.Wrap(err, "unable to parse apiserver version")
+		}
+		if reqVer.Compare(apiserverVer) != 0 {
+			return errors.Errorf("apiserver version mismatch: expected %v, got %v", reqVer.String(), apiserverVer.String())
+		}
+	}
+
+	return nil
+}
+
+func parseContainerImageVersion(image string) (*semver.Version, error) {
+	ver := strings.Split(image, ":")
+	if len(ver) != 2 {
+		return nil, errors.Errorf("invalid container image format: %s", image)
+	}
+	return semver.NewVersion(ver[1])
 }
