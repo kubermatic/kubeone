9a79447f830c7fc4fc7fc264e0ef0e1c30061c17
Marko Mudrinić <mudrinic.mare@gmail.com>
2019-04-03 01:32:55 +0200

    Increase E2E tests timeouts
    * Waiting for nodes to become ready timeout increased to 10 minutes from 5 minutes
    * Waiting for nodes to be upgraded timeout increased to 20 minutes from 10 minutes
    * Conformance E2E tests timeout increased to 1 hour from 30 minutes
    * Upgrades E2E tests timeout increased to 2 hours from 1 hour
    
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- test/e2e/upgrade_test.go
+++ test/e2e/upgrade_test.go
@@ -194,7 +194,7 @@ func waitForNodesUpgraded(client dynclient.Client, targetVersion string) error {
 		return errors.Wrap(err, "desired version is invalid")
 	}
 
-	return wait.Poll(5*time.Second, 10*time.Minute, func() (bool, error) {
+	return wait.Poll(5*time.Second, 20*time.Minute, func() (bool, error) {
 		nodes := corev1.NodeList{}
 		err := client.List(context.Background(), &dynclient.ListOptions{}, &nodes)
 		if err != nil {
