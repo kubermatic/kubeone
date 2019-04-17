e5a27a92e0b777cbd38423a05688796f35b69a44
Artiom Diomin <kron82@gmail.com>
2019-04-17 15:35:41 +0300

    aws private network
    Signed-off-by: Artiom Diomin <kron82@gmail.com>

---
+++ examples/terraform/aws-private/README.md
@@ -0,0 +1,30 @@
+## Assumptions
+
+* VPC (default or custom) with attached internet gateway exists
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|:----:|:-----:|:-----:|
+| aws\_region | AWS region to speak to | string | `"eu-west-3"` | no |
+| cluster\_name | prefix for cloud resources | string | n/a | yes |
+| control\_plane\_type | AWS instance type | string | `"t3.medium"` | no |
+| ssh\_agent\_socket | SSH Agent socket, default to grab from $SSH_AUTH_SOCK | string | `"env:SSH_AUTH_SOCK"` | no |
+| ssh\_port | SSH port | string | `"22"` | no |
+| ssh\_private\_key\_file | SSH private key file, only specify in absence of SSH agent | string | `""` | no |
+| ssh\_public\_key\_file | SSH public key file | string | `"~/.ssh/id_rsa.pub"` | no |
+| ssh\_username | SSH user, used only in output | string | `"ubuntu"` | no |
+| subnet\_netmask\_bits | default 8 bits in /16 CIDR, makes it /24 subnetworks | string | `"8"` | no |
+| subnet\_offset | subnet offset (from main VPC cidr_block) number to be cut | string | `"0"` | no |
+| vpc\_id | VPC to use ('default' for default VPC) | string | `"default"` | no |
+| worker\_type | instance type for workers | string | `"t3.medium"` | no |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| kubeone\_api |  |
+| kubeone\_bastion |  |
+| kubeone\_hosts |  |
+| kubeone\_workers |  |
+
---
+++ examples/terraform/aws-private/main.tf
@@ -0,0 +1,365 @@
+/*
+Copyright 2019 The KubeOne Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+resource "aws_default_vpc" "default" {}
+
+locals {
+  kube_cluster_tag = "kubernetes.io/cluster/${var.cluster_name}"
+}
+
+################################# DATA SOURCES #################################
+data "aws_vpc" "selected" {
+  id = "${var.vpc_id == "default" ? aws_default_vpc.default.id : var.vpc_id}"
+}
+
+data "aws_internet_gateway" "default" {
+  filter {
+    name   = "attachment.vpc-id"
+    values = ["${data.aws_vpc.selected.id}"]
+  }
+}
+
+data "aws_availability_zones" "available" {}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+############################### NETWORKING SETUP ###############################
+
+resource "aws_eip" "nat" {
+  count = "${length(data.aws_availability_zones.available.names)}"
+  vpc   = true
+}
+
+resource "aws_subnet" "public" {
+  count                   = "${length(data.aws_availability_zones.available.names)}"
+  availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
+  cidr_block              = "${cidrsubnet(data.aws_vpc.selected.cidr_block, var.subnet_netmask_bits, var.subnet_offset + count.index)}"
+  map_public_ip_on_launch = true
+  vpc_id                  = "${data.aws_vpc.selected.id}"
+
+  tags {
+    Name    = "${var.cluster_name}_public_${data.aws_availability_zones.available.names[count.index]}"
+    Cluster = "${var.cluster_name}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count                   = "${length(data.aws_availability_zones.available.names)}"
+  availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
+  cidr_block              = "${cidrsubnet(data.aws_vpc.selected.cidr_block, var.subnet_netmask_bits, var.subnet_offset + length(aws_subnet.public.*.id) + count.index)}"
+  map_public_ip_on_launch = false
+  vpc_id                  = "${data.aws_vpc.selected.id}"
+
+  tags {
+    Name    = "${var.cluster_name}_private_${data.aws_availability_zones.available.names[count.index]}"
+    Cluster = "${var.cluster_name}"
+  }
+}
+
+resource "aws_nat_gateway" "main" {
+  count         = "${length(data.aws_availability_zones.available.names)}"
+  allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
+  subnet_id     = "${element(aws_subnet.public.*.id, count.index)}"
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = "${data.aws_vpc.selected.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${data.aws_internet_gateway.default.id}"
+  }
+}
+
+resource "aws_route_table" "private" {
+  count  = "${length(data.aws_availability_zones.available.names)}"
+  vpc_id = "${data.aws_vpc.selected.id}"
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = "${element(aws_nat_gateway.main.*.id, count.index)}"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = "${length(data.aws_availability_zones.available.names)}"
+  subnet_id      = "${element(aws_subnet.public.*.id, count.index)}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+
+resource "aws_route_table_association" "private" {
+  count          = "${length(data.aws_availability_zones.available.names)}"
+  subnet_id      = "${element(aws_subnet.private.*.id, count.index)}"
+  route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
+}
+
+################################ LOAD BALLANCER ################################
+
+resource "aws_lb" "control_plane" {
+  name               = "${var.cluster_name}-api-lb"
+  internal           = false
+  load_balancer_type = "network"
+  subnets            = ["${aws_subnet.private.*.id}"]
+
+  tags = "${map(
+    "Name", "${var.cluster_name}-control_plane",
+    "Cluster", "${var.cluster_name}",
+    "${local.kube_cluster_tag}", "shared",
+  )}"
+}
+
+resource "aws_lb_target_group" "control_plane_api" {
+  name     = "${var.cluster_name}-api"
+  port     = 6443
+  protocol = "TCP"
+  vpc_id   = "${data.aws_vpc.selected.id}"
+}
+
+resource "aws_lb_listener" "control_plane_api" {
+  load_balancer_arn = "${aws_lb.control_plane.arn}"
+  port              = 6443
+  protocol          = "TCP"
+
+  default_action {
+    target_group_arn = "${aws_lb_target_group.control_plane_api.arn}"
+    type             = "forward"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "control_plane_api" {
+  count            = 3
+  target_group_arn = "${aws_lb_target_group.control_plane_api.arn}"
+  target_id        = "${element(aws_instance.control_plane.*.id, count.index)}"
+  port             = 6443
+}
+
+############################### SECURITY GROUPS ################################
+resource "aws_security_group" "common" {
+  name        = "${var.cluster_name}-common"
+  description = "cluster common rules"
+  vpc_id      = "${data.aws_vpc.selected.id}"
+
+  tags = "${map(
+    "Name", "${var.cluster_name}-common",
+    "Cluster", "${local.kube_cluster_tag}",
+    "${local.kube_cluster_tag}", "shared",
+  )}"
+
+  ingress {
+    from_port   = "${var.ssh_port}"
+    to_port     = "${var.ssh_port}"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "control_plane" {
+  name        = "${var.cluster_name}-control_planes"
+  description = "cluster control_planes"
+  vpc_id      = "${data.aws_vpc.selected.id}"
+
+  tags = "${map(
+    "Name", "${var.cluster_name}-control_plane",
+    "Cluster", "${var.cluster_name}",
+    "${local.kube_cluster_tag}", "shared",
+  )}"
+
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+##################################### IAM ######################################
+resource "aws_iam_role" "control_plane" {
+  name = "${var.cluster_name}-host"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "control_plane" {
+  name = "${var.cluster_name}-control-plane"
+  role = "${aws_iam_role.control_plane.name}"
+}
+
+resource "aws_iam_role_policy" "control_plane" {
+  name = "${var.cluster_name}-control-plane"
+  role = "${aws_iam_role.control_plane.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": ["ec2:*"],
+      "Resource": ["*"]
+    },
+    {
+      "Effect": "Allow",
+      "Action": ["elasticloadbalancing:*"],
+      "Resource": ["*"]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "workers" {
+  name = "${var.cluster_name}-workers"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "workers" {
+  name = "${var.cluster_name}-workers"
+  role = "${aws_iam_role.workers.name}"
+}
+
+resource "aws_iam_role_policy" "workers" {
+  name = "${var.cluster_name}-workers"
+  role = "${aws_iam_role.workers.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+################################## SSH ACCESS ##################################
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "${var.cluster_name}-deployer-key"
+  public_key = "${file("${var.ssh_public_key_file}")}"
+}
+
+################################## INSTANCES ###################################
+
+resource "aws_instance" "control_plane" {
+  count = 3
+
+  tags = "${map(
+    "Cluster", "${var.cluster_name}",
+    "Name", "${var.cluster_name}-control_plane-${count.index + 1}",
+    "${local.kube_cluster_tag}", "shared",
+  )}"
+
+  instance_type          = "${var.control_plane_type}"
+  iam_instance_profile   = "${aws_iam_instance_profile.control_plane.name}"
+  ami                    = "${data.aws_ami.ubuntu.id}"
+  key_name               = "${aws_key_pair.deployer.key_name}"
+  vpc_security_group_ids = ["${aws_security_group.common.id}", "${aws_security_group.control_plane.id}"]
+  availability_zone      = "${data.aws_availability_zones.available.names[count.index]}"
+  subnet_id              = "${element(aws_subnet.private.*.id, count.index)}"
+  ebs_optimized          = true
+
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 100
+  }
+}
+
+resource "aws_instance" "bastion" {
+  tags = "${map(
+    "Cluster", "${var.cluster_name}",
+    "Name", "${var.cluster_name}-control_plane-${count.index + 1}",
+    "${local.kube_cluster_tag}", "shared",
+  )}"
+
+  instance_type          = "t3.nano"
+  ami                    = "${data.aws_ami.ubuntu.id}"
+  key_name               = "${aws_key_pair.deployer.key_name}"
+  vpc_security_group_ids = ["${aws_security_group.common.id}"]
+  availability_zone      = "${data.aws_availability_zones.available.names[count.index]}"
+  subnet_id              = "${element(aws_subnet.public.*.id, count.index)}"
+
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 100
+  }
+}
