f5067336c5b7afaaaf4072a8a44ceb35207cc5aa
Artiom Diomin <kron82@gmail.com>
2019-04-18 14:47:48 +0300

    aws private network example (#355)
    * aws private network
    
    Signed-off-by: Artiom Diomin <kron82@gmail.com>
    
    * replicas = 1
    
    Signed-off-by: Artiom Diomin <kron82@gmail.com>

---
+++ examples/terraform/aws-private/output.tf
@@ -0,0 +1,74 @@
+output "kubeone_api" {
+  value = {
+    endpoint = "${aws_lb.control_plane.dns_name}"
+  }
+}
+
+output "kubeone_bastion" {
+  value = "${aws_instance.bastion.0.public_ip}"
+}
+
+output "kubeone_hosts" {
+  value = {
+    control_plane = {
+      cluster_name         = "${var.cluster_name}"
+      cloud_provider       = "aws"
+      private_address      = "${aws_instance.control_plane.*.private_ip}"
+      ssh_agent_socket     = "${var.ssh_agent_socket}"
+      ssh_port             = "${var.ssh_port}"
+      ssh_private_key_file = "${var.ssh_private_key_file}"
+      ssh_user             = "${var.ssh_username}"
+    }
+  }
+}
+
+output "kubeone_workers" {
+  value = {
+    # following outputs will be parsed by kubeone and automatically merged into
+    # corresponding (by name) worker definition
+    pool-a = {
+      region           = "${var.aws_region}"
+      ami              = "${data.aws_ami.ubuntu.id}"
+      availabilityZone = "${data.aws_availability_zones.available.names[0]}"
+      instanceProfile  = "${aws_iam_instance_profile.workers.name}"
+      securityGroupIDs = ["${aws_security_group.common.id}"]
+      vpcId            = "${data.aws_vpc.selected.id}"
+      subnetId         = "${aws_subnet.private.0.id}"
+      instanceType     = "${var.worker_type}"
+      diskSize         = 100
+      sshPublicKeys    = ["${aws_key_pair.deployer.public_key}"]
+      replicas         = 1
+      operatingSystem  = "ubuntu"
+    }
+
+    pool-b = {
+      region           = "${var.aws_region}"
+      ami              = "${data.aws_ami.ubuntu.id}"
+      availabilityZone = "${data.aws_availability_zones.available.names[1]}"
+      instanceProfile  = "${aws_iam_instance_profile.workers.name}"
+      securityGroupIDs = ["${aws_security_group.common.id}"]
+      vpcId            = "${data.aws_vpc.selected.id}"
+      subnetId         = "${aws_subnet.private.1.id}"
+      instanceType     = "${var.worker_type}"
+      diskSize         = 100
+      sshPublicKeys    = ["${aws_key_pair.deployer.public_key}"]
+      replicas         = 1
+      operatingSystem  = "ubuntu"
+    }
+
+    pool-c = {
+      region           = "${var.aws_region}"
+      ami              = "${data.aws_ami.ubuntu.id}"
+      availabilityZone = "${data.aws_availability_zones.available.names[2]}"
+      instanceProfile  = "${aws_iam_instance_profile.workers.name}"
+      securityGroupIDs = ["${aws_security_group.common.id}"]
+      vpcId            = "${data.aws_vpc.selected.id}"
+      subnetId         = "${aws_subnet.private.2.id}"
+      instanceType     = "${var.worker_type}"
+      diskSize         = 100
+      sshPublicKeys    = ["${aws_key_pair.deployer.public_key}"]
+      replicas         = 1
+      operatingSystem  = "ubuntu"
+    }
+  }
+}
---
+++ examples/terraform/aws-private/variables.tf
@@ -0,0 +1,74 @@
+/*
+Copyright 2019 The KubeOne Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+variable "cluster_name" {
+  description = "prefix for cloud resources"
+}
+
+variable "aws_region" {
+  default     = "eu-west-3"
+  description = "AWS region to speak to"
+}
+
+variable "vpc_id" {
+  default     = "default"
+  description = "VPC to use ('default' for default VPC)"
+}
+
+variable "subnet_offset" {
+  default     = 0
+  description = "subnet offset (from main VPC cidr_block) number to be cut"
+}
+
+variable "subnet_netmask_bits" {
+  default     = 8
+  description = "default 8 bits in /16 CIDR, makes it /24 subnetworks"
+}
+
+variable "ssh_public_key_file" {
+  default     = "~/.ssh/id_rsa.pub"
+  description = "SSH public key file"
+}
+
+variable "ssh_private_key_file" {
+  default     = ""
+  description = "SSH private key file, only specify in absence of SSH agent"
+}
+
+variable "ssh_agent_socket" {
+  description = "SSH Agent socket, default to grab from $SSH_AUTH_SOCK"
+  default     = "env:SSH_AUTH_SOCK"
+}
+
+variable "ssh_port" {
+  description = "SSH port"
+  default     = 22
+}
+
+variable "ssh_username" {
+  default     = "ubuntu"
+  description = "SSH user, used only in output"
+}
+
+variable "control_plane_type" {
+  default     = "t3.medium"
+  description = "AWS instance type"
+}
+
+variable "worker_type" {
+  default     = "t3.medium"
+  description = "instance type for workers"
+}
