7da061f09f52a0d08ef717c83407b6166024dbc5
Marko Mudrinić <mudrinic.mare@gmail.com>
2019-04-19 15:33:01 +0200

    Add the KubeOneCluster API scheme
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

---
+++ pkg/apis/kubeone/scheme/scheme.go
@@ -0,0 +1,45 @@
+/*
+Copyright 2019 The KubeOne Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package scheme
+
+import (
+	"github.com/kubermatic/kubeone/pkg/apis/kubeone"
+	"github.com/kubermatic/kubeone/pkg/apis/kubeone/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+)
+
+// Scheme is a runtime.Scheme object to which all KubeOne API types are registered
+var Scheme = runtime.NewScheme()
+
+// Codecs is a CodecFactory object used to provide encoding and decoding for the scheme
+var Codecs = serializer.NewCodecFactory(Scheme)
+
+func init() {
+	metav1.AddToGroupVersion(Scheme, schema.GroupVersion{Version: "v1"})
+	AddToScheme(Scheme)
+}
+
+// AddToScheme builds the KubeOne scheme
+func AddToScheme(scheme *runtime.Scheme) {
+	utilruntime.Must(kubeone.AddToScheme(scheme))
+	utilruntime.Must(v1alpha1.AddToScheme(scheme))
+	utilruntime.Must(scheme.SetVersionPriority(v1alpha1.SchemeGroupVersion))
+}
