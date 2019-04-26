4f4e49f6b7238172b3077ec3ebc17e06bbbfeeeb
Marko Mudrinić <mudrinic.mare@gmail.com>
2019-04-26 11:38:55 +0200

    Update machine-controller to v1.1.5 (#378)
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- pkg/templates/machinecontroller/deployment.go
+++ pkg/templates/machinecontroller/deployment.go
@@ -43,7 +43,7 @@ const (
 	MachineControllerNamespace             = metav1.NamespaceSystem
 	MachineControllerAppLabelKey           = "app"
 	MachineControllerAppLabelValue         = "machine-controller"
-	MachineControllerTag                   = "v1.1.4"
+	MachineControllerTag                   = "v1.1.5"
 	MachineControllerCredentialsSecretName = "machine-controller-credentials"
 )
 
