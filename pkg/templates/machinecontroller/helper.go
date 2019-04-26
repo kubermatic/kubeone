cfc38e0ccb1d4b3ba8ebf3b644b023e0f17e001c
Marko Mudrinić <mudrinic.mare@gmail.com>
2019-04-26 15:46:28 +0200

    Update pkg/templates/machinecontroller package
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- pkg/templates/machinecontroller/helper.go
+++ pkg/templates/machinecontroller/helper.go
@@ -37,7 +37,7 @@ func simpleCreateOrUpdate(ctx context.Context, client dynclient.Client, obj runt
 
 // Ensure install/update machine-controller
 func Ensure(ctx *util.Context) error {
-	if !*ctx.Cluster.MachineController.Deploy {
+	if !ctx.Cluster.MachineController.Deploy {
 		ctx.Logger.Info("Skipping machine-controller deployment because it was disabled in configuration.")
 		return nil
 	}
