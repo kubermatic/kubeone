90c5e0b4be96f5c5bb136e1e01d71dc73cabf171
Marko Mudrinić <mudrinic.mare@gmail.com>
2019-05-13 12:47:07 +0200

    Update machine-controller to v1.1.7
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- pkg/templates/machinecontroller/deployment.go
+++ pkg/templates/machinecontroller/deployment.go
@@ -44,7 +44,7 @@ const (
 	MachineControllerNamespace     = metav1.NamespaceSystem
 	MachineControllerAppLabelKey   = "app"
 	MachineControllerAppLabelValue = "machine-controller"
-	MachineControllerTag           = "v1.1.5"
+	MachineControllerTag           = "v1.1.7"
 )
 
 // Deploy deploys MachineController deployment with RBAC on the cluster
