93cfd5a5b5721ddeb797cb16a0c9632fb9f166a1
mrIncompetent <henrik@henrik-schmidt.de>
2019-08-22 10:03:38 +0200

    update the machine-controller to v1.5.3
    Signed-off-by: mrIncompetent <henrik@henrik-schmidt.de>

--- pkg/templates/machinecontroller/deployment.go
+++ pkg/templates/machinecontroller/deployment.go
@@ -47,7 +47,7 @@ const (
 	MachineControllerNamespace     = metav1.NamespaceSystem
 	MachineControllerAppLabelKey   = "app"
 	MachineControllerAppLabelValue = "machine-controller"
-	MachineControllerTag           = "v1.5.2"
+	MachineControllerTag           = "v1.5.3"
 )
 
 // Deploy deploys MachineController deployment with RBAC on the cluster
