f9edfe83848b6d7871fd41c36654c5c9637efa17
Marko Mudrinić <mudrinic.mare@gmail.com>
2019-09-05 12:29:21 +0200

    Set hostname for GCE instances in Terraform scripts (#607)

--- examples/terraform/gce/output.tf
+++ examples/terraform/gce/output.tf
@@ -31,6 +31,7 @@ output "kubeone_hosts" {
       cloud_provider       = "gce"
       private_address      = google_compute_instance.control_plane.*.network_interface.0.network_ip
       public_address       = google_compute_instance.control_plane.*.network_interface.0.access_config.0.nat_ip
+      hostnames            = google_compute_instance.control_plane.*.name
       ssh_agent_socket     = var.ssh_agent_socket
       ssh_port             = var.ssh_port
       ssh_private_key_file = var.ssh_private_key_file
