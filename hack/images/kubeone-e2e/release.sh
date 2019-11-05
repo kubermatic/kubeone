a39f1ede9acc6a2ced7c640ca4c6a1bc342f0c4e
Artiom Diomin <artiom@loodse.com>
2019-11-05 18:08:55 +0100

    add LABELs to the image
    Signed-off-by: Artiom Diomin <artiom@loodse.com>

--- hack/images/kubeone-e2e/release.sh
+++ hack/images/kubeone-e2e/release.sh
@@ -16,7 +16,7 @@
 
 set -euox pipefail
 
-TAG=v0.1.0
+TAG=v0.1.1
 
-docker build --pull -t kubermatic/kubeone-e2e:${TAG} .
+docker build --build-arg version=${TAG} --pull -t kubermatic/kubeone-e2e:${TAG} .
 docker push kubermatic/kubeone-e2e:${TAG}
