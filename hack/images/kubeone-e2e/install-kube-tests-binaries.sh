ce30708bf2ec18d91c7a160bce6ef411a462da94
Artiom Diomin <kron82@gmail.com>
2019-11-06 14:20:18 +0100

    e2e image (#731)
    * e2e image
    
    Signed-off-by: Artiom Diomin <artiom@loodse.com>
    
    * kubeone-e2e image release.sh script
    
    Signed-off-by: Artiom Diomin <artiom@loodse.com>
    
    * add missing boilerplate
    
    Signed-off-by: Artiom Diomin <artiom@loodse.com>
    
    * add LABELs to the image
    
    Signed-off-by: Artiom Diomin <artiom@loodse.com>
    
    * add coreutils to kubermatic/kubeone-e2e
    
    Signed-off-by: Artiom Diomin <artiom@loodse.com>
    
    * install openssh-keygen and client into e2e image
    
    Signed-off-by: Artiom Diomin <artiom@loodse.com>
    
    * Fix E2E tests for Go 1.13

---
+++ hack/images/kubeone-e2e/install-kube-tests-binaries.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# Copyright 2019 The KubeOne Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -euox pipefail
+
+declare -A full_versions
+full_versions["1.14"]="v1.14.8"
+full_versions["1.15"]="v1.15.5"
+full_versions["1.16"]="v1.16.2"
+
+root_dir=${KUBE_TEST_DIR:-"/opt/kube-test"}
+
+for version in "${!full_versions[@]}"; do
+    directory="${root_dir}/kubernetes-${version}"
+    if [[ ! -d "${directory}" ]]; then
+        full_version="${full_versions[${version}]}"
+        mkdir -p ${directory}
+        cd ${directory}
+        kubetest --extract=${full_version}
+        cd -
+
+        find ${directory} -name "*.tar.gz" -type f -delete
+        rm -r ${directory}/kubernetes/cluster
+        rm ${directory}/kubernetes/platforms/linux/amd64/gendocs
+        rm ${directory}/kubernetes/platforms/linux/amd64/genkubedocs
+        rm ${directory}/kubernetes/platforms/linux/amd64/genman
+        rm ${directory}/kubernetes/platforms/linux/amd64/genswaggertypedocs
+        rm ${directory}/kubernetes/platforms/linux/amd64/genyaml
+        rm ${directory}/kubernetes/platforms/linux/amd64/kubemark
+        rm ${directory}/kubernetes/platforms/linux/amd64/linkcheck
+        if [ $(command -v upx) ]; then
+            upx ${directory}/kubernetes/platforms/linux/amd64/*
+        fi
+    fi
+done
+
+rm -rf /tmp/get-kube*
--- test/e2e/kubetest.go
+++ test/e2e/kubetest.go
@@ -56,15 +56,11 @@ func (p *Kubetest) Verify(scenario string) error {
 	}
 
 	k8sVersionPath := fmt.Sprintf("%s/kubernetes-%s/kubernetes/version", p.kubetestDir, p.kubernetesVersion)
-	if _, err := os.Stat(k8sVersionPath); os.IsNotExist(err) {
-		err = getK8sBinaries(p.kubetestDir, p.kubernetesVersion)
-		if err != nil {
-			return err
-		}
+	if _, err := os.Stat(k8sVersionPath); err != nil {
+		return err
 	}
 
 	k8sPath := fmt.Sprintf("%s/kubernetes-%s/kubernetes", p.kubetestDir, p.kubernetesVersion)
-
 	testsArgs := fmt.Sprintf("--test_args=--ginkgo.focus=%s --ginkgo.skip=%s -ginkgo.noColor=true -ginkgo.flakeAttempts=2", scenario, skip)
 
 	_, err := testutil.ExecuteCommand(k8sPath, "kubetest", []string{"--provider=skeleton", "--test", "--ginkgo-parallel", "--check-version-skew=false", testsArgs}, p.envVars)
@@ -72,20 +68,5 @@ func (p *Kubetest) Verify(scenario string) error {
 		return fmt.Errorf("k8s conformnce tests failed: %v", err)
 	}
 
-	return nil
-}
-
-// getK8sBinaries retrieves kubernetes binaries for version
-func getK8sBinaries(kubetestDir string, version string) error {
-	k8sPath := fmt.Sprintf("%s/kubernetes-%s", kubetestDir, version)
-	err := os.MkdirAll(k8sPath, 0755)
-	if err != nil {
-		return fmt.Errorf("unable to create directory %s", k8sPath)
-	}
-
-	_, err = testutil.ExecuteCommand(k8sPath, "kubetest", []string{fmt.Sprintf("--extract=%s", version)}, nil)
-	if err != nil {
-		return fmt.Errorf("getting kubernetes binaries failed: %v", err)
-	}
 	return nil
 }
