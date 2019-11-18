d891ca6bfda46ca2f54febfeda70169a006b53e5
Artiom Diomin <artiom@loodse.com>
2019-11-18 19:10:29 +0200

    Move e2e image to debian
    Signed-off-by: Artiom Diomin <artiom@loodse.com>

--- hack/images/kubeone-e2e/install-kube-tests-binaries.sh
+++ hack/images/kubeone-e2e/install-kube-tests-binaries.sh
@@ -27,21 +27,21 @@ for version in "${!full_versions[@]}"; do
     full_version="${full_versions[${version}]}"
     directory="${root_dir}/kubernetes-${version}"
     if [[ ! -d "${directory}" ]]; then
-        mkdir -p ${directory}
-        cd ${directory}
-        kubetest --extract=${full_version}
+        mkdir -p "${directory}"
+        cd "${directory}"
+        kubetest --extract="${full_version}"
         cd -
 
-        find ${directory} -name "*.tar.gz" -type f -delete
-        rm ${directory}/kubernetes/platforms/linux/amd64/gendocs
-        rm ${directory}/kubernetes/platforms/linux/amd64/genkubedocs
-        rm ${directory}/kubernetes/platforms/linux/amd64/genman
-        rm ${directory}/kubernetes/platforms/linux/amd64/genswaggertypedocs
-        rm ${directory}/kubernetes/platforms/linux/amd64/genyaml
-        rm ${directory}/kubernetes/platforms/linux/amd64/kubemark
-        rm ${directory}/kubernetes/platforms/linux/amd64/linkcheck
-        if [ $(command -v upx) ]; then
-            upx ${directory}/kubernetes/platforms/linux/amd64/*
+        find "${directory}" -name "*.tar.gz" -type f -delete
+        rm "${directory}"/kubernetes/platforms/linux/amd64/gendocs
+        rm "${directory}"/kubernetes/platforms/linux/amd64/genkubedocs
+        rm "${directory}"/kubernetes/platforms/linux/amd64/genman
+        rm "${directory}"/kubernetes/platforms/linux/amd64/genswaggertypedocs
+        rm "${directory}"/kubernetes/platforms/linux/amd64/genyaml
+        rm "${directory}"/kubernetes/platforms/linux/amd64/kubemark
+        rm "${directory}"/kubernetes/platforms/linux/amd64/linkcheck
+        if [ "$(command -v upx)" ]; then
+            upx "${directory}"/kubernetes/platforms/linux/amd64/*
         fi
     fi
 done
