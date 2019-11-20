a20509844ff516b6d94d9b27894f13e26f4686fe
Artiom Diomin <artiom@loodse.com>
2019-11-20 15:32:43 +0200

    Fix vSphere credentials secret
    Signed-off-by: Artiom Diomin <artiom@loodse.com>

--- pkg/credentials/credentials.go
+++ pkg/credentials/credentials.go
@@ -137,13 +137,14 @@ func ProviderCredentials(p kubeone.CloudProviderName, credentialsFilePath string
 			return nil, errors.WithStack(err)
 		}
 
+		vcenterPrefix := vscreds[VSphereAddressMC]
+
 		// force scheme, as machine-controller requires it while terraform does not
 		vscreds[VSphereAddressMC] = "https://" + vscreds[VSphereAddressMC]
 
 		// Save credentials in Secret and configure vSphere cloud controller
 		// manager to read it, in replace of storing those in /etc/kubernates/cloud-config
 		// see more: https://vmware.github.io/vsphere-storage-for-kubernetes/documentation/k8s-secret.html
-		vcenterPrefix := vscreds[VSphereAddressMC]
 		vscreds[fmt.Sprintf("%s.username", vcenterPrefix)] = vscreds[VSphereUsernameMC]
 		vscreds[fmt.Sprintf("%s.password", vcenterPrefix)] = vscreds[VSpherePassword]
 		return vscreds, nil
