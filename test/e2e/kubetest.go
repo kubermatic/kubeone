383fc27b437913d456ddd7b7b57ca7661c78fedc
Artiom Diomin <artiom@loodse.com>
2019-11-25 23:59:28 +0200

    use -ginkgo.v
    Signed-off-by: Artiom Diomin <artiom@loodse.com>

--- test/e2e/kubetest.go
+++ test/e2e/kubetest.go
@@ -63,7 +63,7 @@ func (p *Kubetest) Verify(scenario string) error {
 		p.kubernetesVersion = fmt.Sprintf("v%s", p.kubernetesVersion)
 	}
 
-	testsArgs := fmt.Sprintf(`--test_args="-test.v -ginkgo.focus=%s -ginkgo.skip=%s -ginkgo.noColor=true -ginkgo.flakeAttempts=2"`, scenario, skip)
+	testsArgs := fmt.Sprintf(`--test_args="-test.v -ginkgo.v -ginkgo.focus=%s -ginkgo.skip=%s -ginkgo.noColor=true -ginkgo.flakeAttempts=2"`, scenario, skip)
 	err = testutil.NewExec("kubetest",
 		testutil.WithArgs(
 			"--provider=skeleton",
