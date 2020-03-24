b4b690205790fb4a10dbc8aba2ce5a182b2a2823
Marko Mudrinić <mudrinic.mare@gmail.com>
2020-03-24 13:44:31 +0100

    Update machine-controller types

--- pkg/terraform/config.go
+++ pkg/terraform/config.go
@@ -227,13 +227,16 @@ func (c *Config) updateAWSWorkerset(existingWorkerSet *kubeonev1alpha1.WorkerCon
 		{key: "diskIops", value: awsCloudConfig.DiskIops},
 		{key: "diskSize", value: awsCloudConfig.DiskSize},
 		{key: "diskType", value: awsCloudConfig.DiskType},
+		{key: "ebsVolumeEncrypted", value: awsCloudConfig.EBSVolumeEncrypted},
 		{key: "instanceProfile", value: awsCloudConfig.InstanceProfile},
 		{key: "instanceType", value: awsCloudConfig.InstanceType},
+		{key: "isSpotInstance", value: awsCloudConfig.IsSpotInstance},
 		{key: "region", value: awsCloudConfig.Region},
 		{key: "securityGroupIDs", value: awsCloudConfig.SecurityGroupIDs},
 		{key: "subnetId", value: awsCloudConfig.SubnetID},
 		{key: "tags", value: awsCloudConfig.Tags},
 		{key: "vpcId", value: awsCloudConfig.VPCID},
+		{key: "rhsmOfflineToken", value: awsCloudConfig.RHSMOfflineToken},
 	}
 
 	for _, flag := range flags {
@@ -263,6 +266,10 @@ func (c *Config) updateAzureWorkerset(existingWorkerSet *kubeonev1alpha1.WorkerC
 		{key: "tags", value: azureCloudConfig.Tags},
 		{key: "vmSize", value: azureCloudConfig.VMSize},
 		{key: "vnetName", value: azureCloudConfig.VNetName},
+		{key: "imageID", value: azureCloudConfig.ImageID},
+		{key: "osDiskSize", value: azureCloudConfig.OSDiskSize},
+		{key: "dataDiskSize", value: azureCloudConfig.DataDiskSize},
+		{key: "rhsmOfflineToken", value: azureCloudConfig.RHSMOfflineToken},
 	}
 
 	for _, flag := range flags {
@@ -294,6 +301,8 @@ func (c *Config) updateGCEWorkerset(existingWorkerSet *kubeonev1alpha1.WorkerCon
 		{key: "tags", value: gceCloudConfig.Tags},
 		{key: "multizone", value: gceCloudConfig.MultiZone},
 		{key: "regional", value: gceCloudConfig.Regional},
+		{key: "customImage", value: gceCloudConfig.CustomImage},
+		{key: "rhsmOfflineToken", value: gceCloudConfig.RHSMOfflineToken},
 	}
 
 	for _, flag := range flags {
@@ -373,6 +382,8 @@ func (c *Config) updateOpenStackWorkerset(existingWorkerSet *kubeonev1alpha1.Wor
 		{key: "rootDiskSizeGB", value: openstackConfig.RootDiskSizeGB},
 		{key: "nodeVolumeAttachLimit", value: openstackConfig.NodeVolumeAttachLimit},
 		{key: "tags", value: openstackConfig.Tags},
+		{key: "trustDevicePath", value: openstackConfig.TrustDevicePath},
+		{key: "rhsmOfflineToken", value: openstackConfig.RHSMOfflineToken},
 	}
 
 	for _, flag := range flags {
@@ -395,6 +406,8 @@ func (c *Config) updatePacketWorkerset(existingWorkerSet *kubeonev1alpha1.Worker
 		{key: "projectID", value: packetConfig.ProjectID},
 		{key: "facilities", value: packetConfig.Facilities},
 		{key: "instanceType", value: packetConfig.InstanceType},
+		{key: "billingCycle", value: packetConfig.BillingCycle},
+		{key: "tags", value: packetConfig.Tags},
 	}
 
 	for _, flag := range flags {
@@ -419,11 +432,13 @@ func (c *Config) updateVSphereWorkerset(existingWorkerSet *kubeonev1alpha1.Worke
 		{key: "cpus", value: vsphereConfig.CPUs},
 		{key: "datacenter", value: vsphereConfig.Datacenter},
 		{key: "datastore", value: vsphereConfig.Datastore},
+		{key: "datastoreCluster", value: vsphereConfig.DatastoreCluster},
 		{key: "diskSizeGB", value: vsphereConfig.DiskSizeGB},
 		{key: "folder", value: vsphereConfig.Folder},
 		{key: "memoryMB", value: vsphereConfig.MemoryMB},
 		{key: "templateVMName", value: vsphereConfig.TemplateVMName},
 		{key: "vmNetName", value: vsphereConfig.VMNetName},
+		{key: "rhsmOfflineToken", value: vsphereConfig.RHSMOfflineToken},
 	}
 
 	for _, flag := range flags {
