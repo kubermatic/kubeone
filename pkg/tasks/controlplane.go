7cc0479cb9ffcc10d61dc3b46a5cb523eae3f71c
Artiom Diomin <artiom@loodse.com>
2020-05-02 02:14:10 +0300

    Automatically delete dead etcd members and Node objects
    Signed-off-by: Artiom Diomin <artiom@loodse.com>

--- pkg/tasks/controlplane.go
+++ pkg/tasks/controlplane.go
@@ -33,7 +33,7 @@ func joinControlplaneNode(s *state.State) error {
 func joinControlPlaneNodeInternal(s *state.State, node *kubeoneapi.HostConfig, conn ssh.Connection) error {
 	logger := s.Logger.WithField("node", node.PublicAddress)
 
-	sleepTime := 120 * time.Second
+	sleepTime := 15 * time.Second
 	logger.Infof("Waiting %s to ensure main control plane components are up…", sleepTime)
 	time.Sleep(sleepTime)
 
