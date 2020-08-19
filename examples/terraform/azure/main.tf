3c29ccdb9ccc56b050a427af835ef8fac889bf0b
Artiom Diomin <artiom@loodse.com>
2020-08-19 12:33:43 +0300

    Fix azure terraform (#1059)
    Signed-off-by: Artiom Diomin <kron82@gmail.com>

--- examples/terraform/azure/main.tf
+++ examples/terraform/azure/main.tf
@@ -15,6 +15,7 @@ limitations under the License.
 */
 
 provider "azurerm" {
+  features {}
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -57,7 +58,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "${var.cluster_name}-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vpc.name
-  address_prefix       = "172.16.1.0/24"
+  address_prefixes     = ["172.16.1.0/24"]
 }
 
 resource "azurerm_network_security_group" "sg" {
