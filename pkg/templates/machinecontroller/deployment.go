d921bdaa00eab12fbc33abc2a3aa0d809eedd2c3
Artiom Diomin <kron82@gmail.com>
2020-09-21 12:09:16 +0300

    Update machinecontroller to v1.17.2
    Signed-off-by: Artiom Diomin <kron82@gmail.com>

--- pkg/templates/machinecontroller/deployment.go
+++ pkg/templates/machinecontroller/deployment.go
@@ -46,7 +46,7 @@ const (
 	MachineControllerNamespace     = metav1.NamespaceSystem
 	MachineControllerAppLabelKey   = "app"
 	MachineControllerAppLabelValue = "machine-controller"
-	MachineControllerTag           = "v1.17.1"
+	MachineControllerTag           = "v1.17.2"
 )
 
 func CRDs() []runtime.Object {
