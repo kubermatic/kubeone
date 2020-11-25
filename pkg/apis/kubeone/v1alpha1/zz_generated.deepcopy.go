ae0a457154873641e907c678e6f945d7fa8234c3
Florian Fürstenberg <florian.fuerstenberg@t-systems.com>
2020-11-25 15:55:50 +0100

    Removed new v1beta1 .DynamicWorkers.Config.Annotations field, because v1alpha1 is frozen
    Signed-off-by: Florian Fürstenberg <florian.fuerstenberg@t-systems.com>

--- pkg/apis/kubeone/v1alpha1/zz_generated.deepcopy.go
+++ pkg/apis/kubeone/v1alpha1/zz_generated.deepcopy.go
@@ -441,13 +441,6 @@ func (in *ProviderSpec) DeepCopyInto(out *ProviderSpec) {
 		*out = make(json.RawMessage, len(*in))
 		copy(*out, *in)
 	}
-	if in.Annotations != nil {
-		in, out := &in.Annotations, &out.Annotations
-		*out = make(map[string]string, len(*in))
-		for key, val := range *in {
-			(*out)[key] = val
-		}
-	}
 	if in.Labels != nil {
 		in, out := &in.Labels, &out.Labels
 		*out = make(map[string]string, len(*in))
