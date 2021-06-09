efe62ff17aab9bac7031e87d1f5a61d9c0d0131c
nerdeveloper <odirionye@gmail.com>
2021-06-09 12:08:26 +0100

    chore: add value types for packet
    Signed-off-by: nerdeveloper <odirionye@gmail.com>

--- examples/terraform/packet/variables.tf
+++ examples/terraform/packet/variables.tf
@@ -16,6 +16,7 @@ limitations under the License.
 
 variable "cluster_name" {
   description = "Name of the cluster"
+  type        = string
 }
 
 variable "worker_os" {
@@ -25,31 +26,38 @@ variable "worker_os" {
   # * ubuntu
   # * centos
   default = "ubuntu"
+  type    = string
 }
 
 variable "ssh_public_key_file" {
   description = "SSH public key file"
   default     = "~/.ssh/id_rsa.pub"
+  type        = string
 }
 
 variable "ssh_port" {
   description = "SSH port to be used to provision instances"
   default     = 22
+  type        = number
 }
 
 variable "ssh_username" {
   description = "SSH user, used only in output"
   default     = "root"
+  type        = string
 }
 
 variable "ssh_private_key_file" {
   description = "SSH private key file used to access instances"
   default     = ""
+  type        = string
+
 }
 
 variable "ssh_agent_socket" {
   description = "SSH Agent socket, default to grab from $SSH_AUTH_SOCK"
   default     = "env:SSH_AUTH_SOCK"
+  type        = string
 }
 
 # Provider specific settings
@@ -57,24 +65,29 @@ variable "ssh_agent_socket" {
 variable "facility" {
   default     = "ams1"
   description = "Facility (datacenter)"
+  type        = string
 }
 
 variable "control_plane_operating_system" {
   default     = "ubuntu_18_04"
   description = "Image to use for control plane provisioning"
+  type        = string
 }
 
 variable "lb_operating_system" {
   default     = "ubuntu_18_04"
   description = "Image to use for loadbalancer provisioning"
+  type        = string
 }
 
 variable "device_type" {
   default     = "t1.small.x86"
   description = "type (size) of the device"
+  type        = string
 }
 
 variable "project_id" {
   description = "project ID"
+  type        = string
 }
 
