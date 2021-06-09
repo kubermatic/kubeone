aa70962318080f5b3d8444f3db04cf318df6ec3c
nerdeveloper <odirionye@gmail.com>
2021-06-09 11:58:07 +0100

    chore: add value types for azure
    Signed-off-by: nerdeveloper <odirionye@gmail.com>

--- examples/terraform/azure/variables.tf
+++ examples/terraform/azure/variables.tf
@@ -16,6 +16,7 @@ limitations under the License.
 
 variable "cluster_name" {
   description = "Name of the cluster"
+  type        = string
 }
 
 variable "worker_os" {
@@ -25,31 +26,37 @@ variable "worker_os" {
   # * ubuntu
   # * centos
   default = "ubuntu"
+  type    = string
 }
 
 variable "ssh_public_key_file" {
   description = "SSH public key file"
   default     = "~/.ssh/id_rsa.pub"
+  type        = string
 }
 
 variable "ssh_port" {
   description = "SSH port to be used to provision instances"
   default     = 22
+  type        = number
 }
 
 variable "ssh_username" {
   description = "SSH user, used only in output"
   default     = "ubuntu"
+  type        = string
 }
 
 variable "ssh_private_key_file" {
   description = "SSH private key file used to access instances"
   default     = ""
+  type        = string
 }
 
 variable "ssh_agent_socket" {
   description = "SSH Agent socket, default to grab from $SSH_AUTH_SOCK"
   default     = "env:SSH_AUTH_SOCK"
+  type        = string
 }
 
 # Provider specific settings
@@ -57,24 +64,29 @@ variable "ssh_agent_socket" {
 variable "location" {
   description = "Azure datacenter to use"
   default     = "westeurope"
+  type        = string
 }
 
 variable "control_plane_vm_size" {
   description = "VM Size for control plane machines"
   default     = "Standard_B2s"
+  type        = string
 }
 
 variable "worker_vm_size" {
   description = "VM Size for worker machines"
   default     = "Standard_B2s"
+  type        = string
 }
 
 variable "control_plane_vm_count" {
   description = "Number of control plane instances"
   default     = 3
+  type        = number
 }
 
 variable "initial_machinedeployment_replicas" {
   description = "Number of replicas per MachineDeployment"
   default     = 1
+  type        = number
 }
