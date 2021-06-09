d6bbecdb790ff8c4d99252fda7bc729f45a2532d
nerdeveloper <odirionye@gmail.com>
2021-06-09 12:04:47 +0100

    chore: add value types for hetzner
    Signed-off-by: nerdeveloper <odirionye@gmail.com>

--- examples/terraform/hetzner/variables.tf
+++ examples/terraform/hetzner/variables.tf
@@ -16,6 +16,7 @@ limitations under the License.
 
 variable "cluster_name" {
   description = "prefix for cloud resources"
+  type        = string
 }
 
 variable "worker_os" {
@@ -25,65 +26,79 @@ variable "worker_os" {
   # * ubuntu
   # * centos
   default = "ubuntu"
+  type    = string
 }
 
 variable "ssh_public_key_file" {
   description = "SSH public key file"
   default     = "~/.ssh/id_rsa.pub"
+  type        = string
 }
 
 variable "ssh_port" {
   description = "SSH port to be used to provision instances"
   default     = 22
+  type        = string
 }
 
 variable "ssh_username" {
   description = "SSH user, used only in output"
   default     = "root"
+  type        = string
 }
 
 variable "ssh_private_key_file" {
   description = "SSH private key file used to access instances"
   default     = ""
+  type        = string
 }
 
 variable "ssh_agent_socket" {
   description = "SSH Agent socket, default to grab from $SSH_AUTH_SOCK"
   default     = "env:SSH_AUTH_SOCK"
+  type        = string
 }
 
 # Provider specific settings
 
 variable "control_plane_type" {
   default = "cx21"
+  type    = string
 }
 
 variable "worker_type" {
   default = "cx21"
+  type    = string
 }
 
 variable "workers_replicas" {
   default = 1
+  type    = number
 }
 
 variable "lb_type" {
   default = "lb11"
+  type    = string
 }
 
 variable "datacenter" {
   default = "nbg1"
+  type    = string
 }
 
 variable "image" {
   default = "ubuntu-20.04"
+  type    = string
 }
 
 variable "ip_range" {
   default     = "192.168.0.0/16"
   description = "ip range to use for private network"
+  type        = string
 }
 
 variable "network_zone" {
   default     = "eu-central"
   description = "network zone to use for private network"
+  type        = string
 }
