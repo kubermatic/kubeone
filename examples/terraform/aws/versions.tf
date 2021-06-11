3e60a3eef78e865edfe07c7f5b6462020bce4731
Artiom Diomin <kron82@gmail.com>
2021-06-11 13:06:17 +0300

    Fix AWS terraform config
    Signed-off-by: Artiom Diomin <kron82@gmail.com>

--- examples/terraform/aws/versions.tf
+++ examples/terraform/aws/versions.tf
@@ -1,3 +1,9 @@
 terraform {
-  required_version = ">= 0.12.10"
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.45.0"
+    }
+  }
 }
