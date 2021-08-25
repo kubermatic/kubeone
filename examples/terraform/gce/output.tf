44b48f4334f49eba6d071e5255928e1c766718c5
Ammar Lakis <ammar.lakis@gmail.com>
2021-08-25 11:17:01 +0200

    Add output for kubeone manifest and machine deployment
    Signed-off-by: Ammar Lakis <ammar.lakis@gmail.com>

--- examples/terraform/gce/output.tf
+++ examples/terraform/gce/output.tf
@@ -14,70 +14,118 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-output "kubeone_api" {
-  description = "kube-apiserver LB endpoint"
-
-  value = {
-    endpoint = google_compute_address.lb_ip.address
-  }
-}
-
-output "kubeone_hosts" {
-  description = "Control plane endpoints to SSH to"
-
-  value = {
-    control_plane = {
-      cluster_name         = var.cluster_name
-      cloud_provider       = "gce"
-      private_address      = google_compute_instance.control_plane.*.network_interface.0.network_ip
-      public_address       = google_compute_instance.control_plane.*.network_interface.0.access_config.0.nat_ip
-      hostnames            = google_compute_instance.control_plane.*.name
-      ssh_agent_socket     = var.ssh_agent_socket
-      ssh_port             = var.ssh_port
-      ssh_private_key_file = var.ssh_private_key_file
-      ssh_user             = var.ssh_username
+output "kubeone" {
+  value = yamlencode({
+    apiVersion : "kubeone.io/v1beta1"
+    kind : "KubeOneCluster"
+    name : "k1"
+    versions : {
+      kubernetes : "1.21.0"
     }
-  }
+    controlPlane : {
+      hosts : [for host in google_compute_instance.control_plane :
+        {
+          publicAddress : host.network_interface.0.access_config.0.nat_ip
+          privateAddress : host.network_interface.0.network_ip
+          sshUsername : "root"
+        }
+      ]
+    }
+    staticWorkers : {
+      hosts : [for host in google_compute_instance.static_workers :
+        {
+          publicAddress : host.network_interface.0.access_config.0.nat_ip
+          privateAddress : host.network_interface.0.network_ip
+          sshUsername : "root"
+        }
+      ]
+    }
+    cloudProvider : {
+      gcp : {}
+    }
+
+  })
 }
 
-output "kubeone_workers" {
-  description = "Workers definitions, that will be transformed into MachineDeployment object"
-
-  value = {
-    # following outputs will be parsed by kubeone and automatically merged into
-    # corresponding (by name) worker definition
-    "${var.cluster_name}-pool1" = {
-      replicas = 1
-      providerSpec = {
-        sshPublicKeys   = [file(var.ssh_public_key_file)]
-        operatingSystem = var.worker_os
-        operatingSystemSpec = {
-          distUpgradeOnBoot = false
+output "master_machine_deployment" {
+  value = yamlencode({
+    apiVersion : "cluster.k8s.io/v1alpha1"
+    kind : "MachineDeployment"
+    metadata : {
+      name : "kkp-master-pool-az-a"
+      namespace : "kube-system"
+      annotations : {
+        "machinedeployment.clusters.k8s.io/revision" : "1"
+        "cluster.k8s.io/cluster-api-autoscaler-node-group-min-size" : "1"
+        "cluster.k8s.io/cluster-api-autoscaler-node-group-max-size" : "5"
+      }
+    }
+    spec : {
+      minReadySeconds : 0
+      progressDeadlineSeconds : 600
+      replicas : 1
+      revisionHistoryLimit : 1
+      selector : {
+        matchLabels : {
+          workerset : "kkp-master-pool-az-a"
         }
-        cloudProviderSpec = {
-          # provider specific fields:
-          # see example under `cloudProviderSpec` section at:
-          # https://github.com/kubermatic/machine-controller/blob/master/examples/gce-machinedeployment.yaml
-          diskSize              = 50
-          diskType              = "pd-ssd"
-          machineType           = var.workers_type
-          network               = google_compute_network.network.self_link
-          subnetwork            = google_compute_subnetwork.subnet.self_link
-          zone                  = data.google_compute_zones.available.names[0]
-          preemptible           = false
-          assignPublicIPAddress = true
-          # Enable support for multizone clusters
-          multizone = true
-          labels = {
-            "${var.cluster_name}-workers" = "pool1"
+      }
+      strategy : {
+        rollingUpdate : {
+          maxSurge : 3
+          maxUnavailable : 0
+        }
+        type : "RollingUpdate"
+      }
+      template : {
+        metadata : {
+          creationTimestamp : null
+          labels : {
+            workerset : "kkp-master-pool-az-a"
           }
-          tags     = ["firewall", "targets", "${var.cluster_name}-pool1"]
-          regional = false
-          # Use custom image (optional)
-          # customImage = ""
+          namespace : "kube-system"
+        }
+        spec : {
+          metadata : {
+            creationTimestamp : null
+            labels : {
+              workerset : "kkp-master-pool-az-a"
+            }
+          }
+          providerSpec : {
+            value : {
+              cloudProvider : "gce"
+              cloudProviderSpec : {
+                assignPublicIPAddress : true
+                diskSize : 20
+                diskType : "pd-ssd"
+                labels : {
+                  kkp-master-workers : "pool-az-a"
+                }
+                machineType : "n1-standard-2"
+                multizone : true
+                network : google_compute_network.network.self_link
+                preemptible : false
+                regional : false
+                subnetwork : google_compute_subnetwork.subnet.self_link
+                tags : [
+                  "firewall"
+                  , "targets"
+                  , "kkp-master-pool-az-a"
+                ]
+                zone : data.google_compute_zones.available.names[0]
+              }
+              operatingSystem : "ubuntu"
+              operatingSystemSpec : {
+                distUpgradeOnBoot : false
+              }
+            }
+          }
+          versions : {
+            kubelet : "1.21.0"
+          }
         }
       }
     }
-  }
+  })
 }
-
