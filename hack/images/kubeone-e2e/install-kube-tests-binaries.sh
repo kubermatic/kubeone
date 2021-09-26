972b7a95d06576d38d9275ea587e02925917bb9d
Marko Mudrinić <mudrinic.mare@gmail.com>
2021-09-26 13:35:09 +0300

    Update the kubeone-e2e image to Go 1.17.1 (#1541)
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- hack/images/kubeone-e2e/install-kube-tests-binaries.sh
+++ hack/images/kubeone-e2e/install-kube-tests-binaries.sh
@@ -17,10 +17,10 @@
 set -euox pipefail
 
 declare -A full_versions
-full_versions["1.19"]="v1.19.14"
-full_versions["1.20"]="v1.20.10"
-full_versions["1.21"]="v1.21.4"
-full_versions["1.22"]="v1.22.0"
+full_versions["1.19"]="v1.19.15"
+full_versions["1.20"]="v1.20.11"
+full_versions["1.21"]="v1.21.5"
+full_versions["1.22"]="v1.22.2"
 
 root_dir=${KUBETESTS_ROOT:-"/opt/kube-test"}
 tmp_root=${TMP_ROOT:-"/tmp/get-kube"}
