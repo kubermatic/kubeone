6e0044c07d80e631a95daa3539893e3c3b8075da
Vijay Dharap <vijay@kubermatic.com>
2021-10-05 14:50:32 +0530

    Updated as per review comments

--- examples/terraform/azure/main.tf
+++ examples/terraform/azure/main.tf
@@ -45,8 +45,8 @@ resource "azurerm_availability_set" "avset" {
   }
 }
 
-resource "azurerm_availability_set" "avset_worker" {
-  name                         = "${var.cluster_name}-avset-worker"
+resource "azurerm_availability_set" "avset_workers" {
+  name                         = "${var.cluster_name}-avset-workers"
   location                     = var.location
   resource_group_name          = azurerm_resource_group.rg.name
   platform_fault_domain_count  = 2
@@ -108,29 +108,7 @@ resource "azurerm_network_security_group" "sg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-  security_rule {
-    name                       = "Azure_LB_to_VMs_https"
-    priority                   = 1002
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "443"
-    source_address_prefix      = "AzureLoadBalancer"
-    destination_address_prefix = "*"
-  }
-  security_rule {
-    name                       = "Azure_LB_to_VMs_http"
-    priority                   = 1003
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "AzureLoadBalancer"
-    destination_address_prefix = "*"
-  }
-
+  
   tags = {
     environment = "kubeone"
     cluster     = var.cluster_name
