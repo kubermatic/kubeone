570dbca5f3937e12f32c593ed4f9fb7f1cafe084
Marko Mudrinić <mudrinic.mare@gmail.com>
2021-12-01 20:05:37 +0100

    Fix boilerplate in the update-docs script
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- hack/ci/update-docs.sh
+++ hack/ci/update-docs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2020 The Kubermatic Kubernetes Platform contributors.
+# Copyright 2021 The KubeOne Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
