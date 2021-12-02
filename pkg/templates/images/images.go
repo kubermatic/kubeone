98b5ee4e277edbd2ffeddf6150990578fc2f36fc
Marko Mudrinić <mudrinic.mare@gmail.com>
2021-12-02 18:09:58 +0100

    [release/v1.3] Update machine-controller to v1.37.2 (#1654)
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- pkg/templates/images/images.go
+++ pkg/templates/images/images.go
@@ -90,7 +90,7 @@ func baseResources() map[Resource]map[string]string {
 		CalicoNode:        {"*": "docker.io/calico/node:v3.19.1"},
 		DNSNodeCache:      {"*": "k8s.gcr.io/k8s-dns-node-cache:1.15.13"},
 		Flannel:           {"*": "quay.io/coreos/flannel:v0.13.0"},
-		MachineController: {"*": "docker.io/kubermatic/machine-controller:v1.35.2"},
+		MachineController: {"*": "docker.io/kubermatic/machine-controller:v1.37.2"},
 		MetricsServer:     {"*": "k8s.gcr.io/metrics-server/metrics-server:v0.5.0"},
 	}
 }
