5386be4c415dff881d5d4708d86cc2476ff881ab
Marko Mudrinić <mudrinic.mare@gmail.com>
2022-01-10 21:43:15 +0100

    Update the config migrate tests
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- pkg/apis/kubeone/config/migrate_test.go
+++ pkg/apis/kubeone/config/migrate_test.go
@@ -35,57 +35,47 @@ var update = flag.Bool("update", false, "update .golden files")
 func TestMigrateOldConfig(t *testing.T) {
 	testcases := []struct {
 		name string
+		err  string
 	}{
 		{
+			name: "config-addons-1",
+		},
+		{
+			name: "config-addons-2",
+		},
+		{
+			name: "config-addons-3",
+		},
+		{
+			name: "config-addons-4",
+		},
+		{
+			name: "config-addons-5",
+		},
+		{
+			name: "config-addons-6",
+		},
+		{
+			name: "config-assetconfig",
+			err:  "the AssetConfiguration API has been removed from the v1beta2 API, please check the docs for information on how to migrate",
+		},
+		{
 			name: "config-aws",
-		},
-		{
-			name: "config-azure",
-		},
-		{
-			name: "config-digitalocean",
-		},
-		{
-			name: "config-gce",
-		},
-		{
-			name: "config-hetzner",
-		},
-		{
-			name: "config-hetzner-networkid",
-		},
-		{
-			name: "config-openstack",
-		},
-		{
-			name: "config-equinix-metal",
-		},
-		{
-			name: "config-vsphere",
-		},
-		{
-			name: "config-none",
 		},
 		{
 			name: "config-full",
 		},
 		{
-			name: "config-full-example",
+			name: "config-packet",
 		},
 		{
-			name: "config-cni-canal",
+			name: "config-podpresets-1",
 		},
 		{
-			name: "config-cni-canal-encrypted",
+			name: "config-podpresets-2",
 		},
 		{
-			name: "config-cni-weave-net",
-		},
-		{
-			name: "config-cni-weave-net-encrypted",
-		},
-		{
-			name: "config-cni-external",
+			name: "config-podpresets-3",
 		},
 	}
 
@@ -94,6 +84,9 @@ func TestMigrateOldConfig(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			newConfigYAML, err := MigrateOldConfig(filepath.Join("testdata", tc.name+"-v1beta1.yaml"))
 			if err != nil {
+				if err.Error() == tc.err {
+					return
+				}
 				t.Errorf("error converting old config: %v", err)
 			}
 
