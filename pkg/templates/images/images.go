a83a02870321fc45871ff80ef2e7c905d7ba094a
Marko Mudrinić <mudrinic.mare@gmail.com>
2022-01-24 09:40:18 +0100

    AWS CCM: Add support for Kubernetes 1.23
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- pkg/templates/images/images.go
+++ pkg/templates/images/images.go
@@ -173,10 +173,11 @@ func optionalResources() map[Resource]map[string]string {
 		},
 
 		AwsCCM: {
-			"1.19.x":    "us.gcr.io/k8s-artifacts-prod/provider-aws/cloud-controller-manager:v1.19.0-alpha.1",
-			"1.20.x":    "us.gcr.io/k8s-artifacts-prod/provider-aws/cloud-controller-manager:v1.20.0-alpha.0",
-			"1.21.x":    "us.gcr.io/k8s-artifacts-prod/provider-aws/cloud-controller-manager:v1.21.0-alpha.0",
-			">= 1.22.0": "us.gcr.io/k8s-artifacts-prod/provider-aws/cloud-controller-manager:v1.22.0-alpha.0",
+			"1.19.x":    "k8s.gcr.io/provider-aws/cloud-controller-manager:v1.19.0-alpha.1",
+			"1.20.x":    "k8s.gcr.io/provider-aws/cloud-controller-manager:v1.20.0-alpha.0",
+			"1.21.x":    "k8s.gcr.io/provider-aws/cloud-controller-manager:v1.21.0-alpha.0",
+			"1.22.x":    "k8s.gcr.io/provider-aws/cloud-controller-manager:v1.22.0-alpha.0",
+			">= 1.23.0": "k8s.gcr.io/provider-aws/cloud-controller-manager:v1.23.0-alpha.0",
 		},
 
 		// Azure CCM
