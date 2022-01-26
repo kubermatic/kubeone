2340768b0afdbcd0f224a7d409c2e9e666bd9041
nerdeveloper <odirionye@gmail.com>
2022-01-27 03:37:41 +0400

    test: add test to check if log size parses

---
+++ pkg/containerruntime/docker_config_test.go
@@ -0,0 +1,56 @@
+package containerruntime
+
+import (
+	"encoding/json"
+	"k8c.io/kubeone/pkg/apis/kubeone"
+	"testing"
+)
+
+func Test_marshalDockerConfig(t *testing.T) {
+	tests := []struct {
+		name    string
+		cluster *kubeone.KubeOneCluster
+		want    string
+	}{
+		{
+			name:    "Should be convert 100Mi to 100m",
+			cluster: genCluster(withContainerLogMaxSize("100Mi")),
+			want:    "100m",
+		},
+		{
+			name:    "Should be convert 100Ki to 100k",
+			cluster: genCluster(withContainerLogMaxSize("100Ki")),
+			want:    "100k",
+		},
+		{
+			name:    "Should be convert 100Gi to 100g",
+			cluster: genCluster(withContainerLogMaxSize("100Gi")),
+			want:    "100g",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := marshalDockerConfig(tt.cluster)
+			if err != nil {
+				t.Errorf("marshalDockerConfig() error = %v,", err)
+				return
+			}
+			cfg := dockerConfig{}
+			err = json.Unmarshal([]byte(got), &cfg)
+			gotLogSize := cfg.LogOpts["max-size"]
+
+			if err != nil {
+				return
+			}
+			if gotLogSize != tt.want {
+				t.Errorf("marshalDockerConfig() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func withContainerLogMaxSize(logSize string) clusterOpts {
+	return func(cls *kubeone.KubeOneCluster) {
+		cls.LoggingConfig.ContainerLogMaxSize = logSize
+	}
+}
