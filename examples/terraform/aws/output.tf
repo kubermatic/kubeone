29886ab6db922aec9c16087ab4c8ca17c81f4d4a
Artiom Diomin <kron82@gmail.com>
2022-02-07 15:02:23 +0200

    Set operating_system in terraform output for static workers as well
    Signed-off-by: Artiom Diomin <kron82@gmail.com>

--- examples/terraform/aws/output.tf
+++ examples/terraform/aws/output.tf
@@ -55,6 +55,7 @@ output "kubeone_static_workers" {
     workers1 = {
       private_address      = aws_instance.static_workers1.*.private_ip
       hostnames            = aws_instance.static_workers1.*.private_dns
+      operating_system     = var.os
       ssh_agent_socket     = var.ssh_agent_socket
       ssh_port             = var.ssh_port
       ssh_private_key_file = var.ssh_private_key_file
