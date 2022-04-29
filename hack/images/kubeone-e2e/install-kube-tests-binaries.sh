ccba6186ca55d4884d55bcbff24d32ad4da1c0ae
Marko Mudrinić <mudrinic.mare@gmail.com>
2022-04-29 11:47:45 +0200

    Update the KubeOne build image to Go 1.18.1 and remove Kubernetes 1.20 jobs and binaries (#2009)
    * Update the KubeOne build image to Go 1.18.1
    
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>
    
    * Remove Kubernetes 1.20 jobs
    
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- hack/images/kubeone-e2e/install-kube-tests-binaries.sh
+++ hack/images/kubeone-e2e/install-kube-tests-binaries.sh
@@ -17,10 +17,9 @@
 set -euox pipefail
 
 declare -A full_versions
-full_versions["1.20"]="v1.20.13"
-full_versions["1.21"]="v1.21.7"
-full_versions["1.22"]="v1.22.4"
-full_versions["1.23"]="v1.23.0"
+full_versions["1.21"]="v1.21.12"
+full_versions["1.22"]="v1.22.9"
+full_versions["1.23"]="v1.23.6"
 
 root_dir=${KUBETESTS_ROOT:-"/opt/kube-test"}
 tmp_root=${TMP_ROOT:-"/tmp/get-kube"}
