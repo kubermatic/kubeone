7a7306e03af0f2fb58eee89b92ab33ee68540800
Marko Mudrinić <mudrinic.mare@gmail.com>
2022-08-24 14:52:54 +0200

    Use c3.medium
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- testv2/e2e/tests_definitions.go
+++ testv2/e2e/tests_definitions.go
@@ -378,8 +378,7 @@ var (
 					"control_plane_operating_system=flatcar_stable",
 					"worker_os=flatcar",
 					"ssh_username=core",
-					"device_type=c3.small.x86", // Flatcar is not available on m3 instances
-					"metro=AM",                 // c3 instances are not available is AM metro
+					"device_type=c3.medium.x86", // Flatcar is not available on m3 instances
 				},
 			},
 		},
