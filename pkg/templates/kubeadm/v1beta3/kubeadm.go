80f33167d994b0099fa4962d55e8973341776a07
Pratik Deoghare <pratik.deoghare@gmail.com>
2022-11-15 11:44:30 +0100

    fix subnet ordering

--- pkg/templates/kubeadm/v1beta3/kubeadm.go
+++ pkg/templates/kubeadm/v1beta3/kubeadm.go
@@ -141,10 +141,12 @@ func NewConfig(s *state.State, host kubeoneapi.HostConfig) ([]runtime.Object, er
 		},
 		Networking: kubeadmv1beta3.Networking{
 			PodSubnet: join(
+				cluster.ClusterNetwork.IPFamily,
 				cluster.ClusterNetwork.PodSubnet,
 				cluster.ClusterNetwork.PodSubnetIPv6,
 			),
 			ServiceSubnet: join(
+				cluster.ClusterNetwork.IPFamily,
 				cluster.ClusterNetwork.ServiceSubnet,
 				cluster.ClusterNetwork.ServiceSubnetIPv6,
 			),
@@ -370,14 +372,19 @@ func addControllerManagerNetworkArgs(m map[string]string, clusterNetwork kubeone
 	}
 }
 
-func join(xs ...string) string {
-	var nonEmpty []string
-	for _, x := range xs {
-		if x != "" {
-			nonEmpty = append(nonEmpty, x)
-		}
+func join(ipFamily kubeoneapi.IPFamily, ipv4Subnet, ipv6Subnet string) string {
+	switch ipFamily {
+	case kubeoneapi.IPFamilyIPv4:
+		return ipv4Subnet
+	case kubeoneapi.IPFamilyIPv6:
+		return ipv6Subnet
+	case kubeoneapi.IPFamilyIPv4IPv6:
+		return strings.Join([]string{ipv4Subnet, ipv6Subnet}, ",")
+	case kubeoneapi.IPFamilyIPv6IPv4:
+		return strings.Join([]string{ipv6Subnet, ipv4Subnet}, ",")
+	default:
+		return "unknown IP family"
 	}
-	return strings.Join(nonEmpty, ",")
 }
 
 // NewConfig returns all required configs to init a cluster via a set of v13 configs
