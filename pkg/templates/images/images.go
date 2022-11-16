e5365222bbf322a9461a252973d60c1a9c09738d
Marko Mudrinić <mudrinic.mare@gmail.com>
2022-11-16 07:57:42 +0100

    Update NodeLocalDNS to v1.22.13 (#2477)
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>
    
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- pkg/templates/images/images.go
+++ pkg/templates/images/images.go
@@ -213,7 +213,7 @@ func baseResources() map[Resource]map[string]string {
 		CalicoCNI:              {"*": "quay.io/calico/cni:v3.23.3"},
 		CalicoController:       {"*": "quay.io/calico/kube-controllers:v3.23.3"},
 		CalicoNode:             {"*": "quay.io/calico/node:v3.23.3"},
-		DNSNodeCache:           {"*": "registry.k8s.io/dns/k8s-dns-node-cache:1.21.1"},
+		DNSNodeCache:           {"*": "registry.k8s.io/dns/k8s-dns-node-cache:1.22.13"},
 		Flannel:                {"*": "quay.io/coreos/flannel:v0.15.1"},
 		MachineController:      {"*": "quay.io/kubermatic/machine-controller:v1.55.0"},
 		MetricsServer:          {"*": "k8s.gcr.io/metrics-server/metrics-server:v0.6.1"},
