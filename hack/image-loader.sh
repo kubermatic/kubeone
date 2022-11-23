0357cd6b359ccc6ba31a532c211ec647190ff800
Marko Mudrinić <mudrinic.mare@gmail.com>
2022-11-23 13:37:59 +0100

    Replace k8s.gcr.io with registry.k8s.io in image-loader script
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- hack/image-loader.sh
+++ hack/image-loader.sh
@@ -109,20 +109,12 @@ k1images=$(kubeone config images list --filter=base --kubernetes-version="$KUBER
 optionalimages=$(kubeone config images list --filter=optional --kubernetes-version="$KUBERNETES_VERSION")
 
 for IMAGE in $k8simages; do
-  # The CoreDNS image has a different override semantics in Kubernetes 1.21 and
-  # in other Kubernetes minor releases (due to a bug).
-  # The image will be overridden in the following way depending on the
-  # Kubernetes version:
-  #   * 1.21: k8s.gcr.io/coredns/coredns -> custom-registry/coredns/coredns
-  #   * all other releases: k8s.gcr.io/coredns/coredns -> custom-registry/coredns
-  if [[ "$IMAGE" == "k8s.gcr.io/coredns/coredns:"* ]]; then
+  # The CoreDNS image has a different override semantics than other images.
+  # The image will be overridden in the following way:
+  #   registry.k8s.io/coredns/coredns -> custom-registry/coredns
+  if [[ "$IMAGE" == "registry.k8s.io/coredns/coredns:"* ]]; then
     corednsVersion=$(cut -d ':' -f 2 <<< "${IMAGE}")
-    kubernetesMinor=$(cut -d '.' -f 2 <<< "${KUBERNETES_VERSION}")
-    if [ "${kubernetesMinor}" -eq "21" ]; then
-      retag "k8s.gcr.io/coredns/coredns:${corednsVersion}" "coredns/coredns"
-    else
-      retag "k8s.gcr.io/coredns/coredns:${corednsVersion}" "coredns"
-    fi
+    retag "registry.k8s.io/coredns/coredns:${corednsVersion}" "coredns"
   else
     retag "${IMAGE}"
   fi
