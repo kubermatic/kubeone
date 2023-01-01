0ee1c4d782efab573e7fc101ef7572c9db13fb25
Marko Mudrinić <mudrinic.mare@gmail.com>
2023-01-01 13:37:47 +0100

    Update Azure CCM to v1.26.0, v1.25.5, v1.24.11, v1.23.24
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- pkg/templates/images/images.go
+++ pkg/templates/images/images.go
@@ -243,14 +243,16 @@ func optionalResources() map[Resource]map[string]string {
 
 		// Azure CCM
 		AzureCCM: {
-			"1.23.x":    "mcr.microsoft.com/oss/kubernetes/azure-cloud-controller-manager:v1.23.21",
-			"1.24.x":    "mcr.microsoft.com/oss/kubernetes/azure-cloud-controller-manager:v1.24.8",
-			">= 1.25.0": "mcr.microsoft.com/oss/kubernetes/azure-cloud-controller-manager:v1.25.3",
+			"1.23.x":    "mcr.microsoft.com/oss/kubernetes/azure-cloud-controller-manager:v1.23.24",
+			"1.24.x":    "mcr.microsoft.com/oss/kubernetes/azure-cloud-controller-manager:v1.24.11",
+			"1.25.x":    "mcr.microsoft.com/oss/kubernetes/azure-cloud-controller-manager:v1.25.5",
+			">= 1.26.0": "mcr.microsoft.com/oss/kubernetes/azure-cloud-controller-manager:v1.26.0",
 		},
 		AzureCNM: {
-			"1.23.x":    "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v1.23.21",
-			"1.24.x":    "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v1.24.8",
-			">= 1.25.0": "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v1.25.3",
+			"1.23.x":    "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v1.23.24",
+			"1.24.x":    "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v1.24.11",
+			"1.25.x":    "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v1.25.5",
+			">= 1.26.0": "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v1.26.0",
 		},
 
 		// AWS EBS CSI driver
