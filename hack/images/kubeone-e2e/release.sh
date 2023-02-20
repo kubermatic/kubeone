f8e571d9345422a140be145948a81d1b2ad6214d
Marko Mudrinić <mudrinic.mare@gmail.com>
2023-02-20 15:54:33 +0100

    Update Go to 1.19.6 (#2649)
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- hack/images/kubeone-e2e/release.sh
+++ hack/images/kubeone-e2e/release.sh
@@ -16,7 +16,7 @@
 
 set -euox pipefail
 
-TAG=v0.1.29
+TAG=v0.1.30
 
 docker build --build-arg version=${TAG} --pull -t quay.io/kubermatic/kubeone-e2e:${TAG} .
 docker push quay.io/kubermatic/kubeone-e2e:${TAG}
