de6f83321c8cd17648d8211dbc6b11b4e658a3dc
pprzekwas <pkprzekwas@gmail.com>
2023-04-06 12:32:20 +0200

    Bump Golang version to 1.20.3 (#2756)

--- hack/images/kubeone-e2e/release.sh
+++ hack/images/kubeone-e2e/release.sh
@@ -16,7 +16,7 @@
 
 set -euox pipefail
 
-TAG=v0.1.30
+TAG=v0.1.31
 
 docker build --build-arg version=${TAG} --pull -t quay.io/kubermatic/kubeone-e2e:${TAG} .
 docker push quay.io/kubermatic/kubeone-e2e:${TAG}
