8db9eeea94c3b4e883fc1d0fee67176e5d7ca37b
Marko Mudrinić <mudrinic.mare@gmail.com>
2023-05-10 17:47:24 +0200

    Replace kubeone-e2e with build image and update Go
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- test/e2e/helpers.go
+++ test/e2e/helpers.go
@@ -55,7 +55,7 @@ import (
 
 const (
 	labelControlPlaneNode = "node-role.kubernetes.io/control-plane"
-	prowImage             = "quay.io/kubermatic/kubeone-e2e:v0.1.32"
+	prowImage             = "quay.io/kubermatic/build:go-1.20-node-18-4"
 	k1CloneURI            = "ssh://git@github.com/kubermatic/kubeone.git"
 )
 
