e0c129e1e3163259dcf867f34d4f9689f7dccc35
Artiom Diomin <artiom@kubermatic.com>
2023-07-05 16:50:15 +0300

    CSI benchmark: give kubelet a list of fine ciphersuites. (#2814)
    Signed-off-by: Artiom Diomin <artiom@kubermatic.com>

--- pkg/templates/kubernetesconfigs/kubelet.go
+++ pkg/templates/kubernetesconfigs/kubelet.go
@@ -17,6 +17,8 @@ limitations under the License.
 package kubernetesconfigs
 
 import (
+	"crypto/tls"
+
 	kubeoneapi "k8c.io/kubeone/pkg/apis/kubeone"
 	"k8c.io/kubeone/pkg/templates/resources"
 
@@ -25,6 +27,32 @@ import (
 	kubeletconfigv1beta1 "k8s.io/kubelet/config/v1beta1"
 )
 
+func SafeTLSCiphers() []string {
+	return []string{
+		tls.CipherSuiteName(tls.TLS_AES_128_GCM_SHA256),
+		tls.CipherSuiteName(tls.TLS_AES_256_GCM_SHA384),
+		tls.CipherSuiteName(tls.TLS_CHACHA20_POLY1305_SHA256),
+		tls.CipherSuiteName(tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA),
+		tls.CipherSuiteName(tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256),
+		tls.CipherSuiteName(tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA),
+		tls.CipherSuiteName(tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384),
+		tls.CipherSuiteName(tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305),
+		tls.CipherSuiteName(tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256),
+		tls.CipherSuiteName(tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA),
+		tls.CipherSuiteName(tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA),
+		tls.CipherSuiteName(tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256),
+		tls.CipherSuiteName(tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA),
+		tls.CipherSuiteName(tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384),
+		tls.CipherSuiteName(tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305),
+		tls.CipherSuiteName(tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256),
+		tls.CipherSuiteName(tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA),
+		tls.CipherSuiteName(tls.TLS_RSA_WITH_AES_128_CBC_SHA),
+		tls.CipherSuiteName(tls.TLS_RSA_WITH_AES_128_GCM_SHA256),
+		tls.CipherSuiteName(tls.TLS_RSA_WITH_AES_256_CBC_SHA),
+		tls.CipherSuiteName(tls.TLS_RSA_WITH_AES_256_GCM_SHA384),
+	}
+}
+
 func NewKubeletConfiguration(cluster *kubeoneapi.KubeOneCluster, featureGates map[string]bool) (runtime.Object, error) {
 	bfalse := false
 	kubeletConfig := &kubeletconfigv1beta1.KubeletConfiguration{
@@ -32,18 +60,19 @@ func NewKubeletConfiguration(cluster *kubeoneapi.KubeOneCluster, featureGates ma
 			APIVersion: "kubelet.config.k8s.io/v1beta1",
 			Kind:       "KubeletConfiguration",
 		},
-		CgroupDriver:         "systemd",
-		ReadOnlyPort:         0,
-		RotateCertificates:   true,
-		ServerTLSBootstrap:   true,
-		ContainerLogMaxSize:  cluster.LoggingConfig.ContainerLogMaxSize,
-		ContainerLogMaxFiles: &cluster.LoggingConfig.ContainerLogMaxFiles,
 		Authentication: kubeletconfigv1beta1.KubeletAuthentication{
 			Anonymous: kubeletconfigv1beta1.KubeletAnonymousAuthentication{
 				Enabled: &bfalse,
 			},
 		},
-		FeatureGates: featureGates,
+		CgroupDriver:         "systemd",
+		ContainerLogMaxFiles: &cluster.LoggingConfig.ContainerLogMaxFiles,
+		ContainerLogMaxSize:  cluster.LoggingConfig.ContainerLogMaxSize,
+		FeatureGates:         featureGates,
+		ReadOnlyPort:         0,
+		RotateCertificates:   true,
+		ServerTLSBootstrap:   true,
+		TLSCipherSuites:      SafeTLSCiphers(),
 	}
 
 	if cluster.Features.NodeLocalDNS.Deploy {
