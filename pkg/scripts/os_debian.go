a26d8aa18d5137a4313a11ce3b6777e7be6086a6
Marko Mudrinić <mudrinic.mare@gmail.com>
2023-09-25 11:44:52 +0200

    Properly handle GPG keys for Docker and k8s repos
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- pkg/scripts/os_debian.go
+++ pkg/scripts/os_debian.go
@@ -61,9 +61,11 @@ sudo systemctl enable --now iscsid
 {{- end }}
 
 {{- if .CONFIGURE_REPOSITORIES }}
-curl -fsSL https://pkgs.k8s.io/core:/stable:/{{ .KUBERNETES_MAJOR_MINOR }}/deb/Release.key | sudo apt-key add -
+sudo install -m 0755 -d /etc/apt/keyrings
 
-echo "deb https://pkgs.k8s.io/core:/stable:/{{ .KUBERNETES_MAJOR_MINOR }}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/{{ .KUBERNETES_MAJOR_MINOR }}/deb/Release.key | sudo gpg --dearmor --yes -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/{{ .KUBERNETES_MAJOR_MINOR }}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update
 {{- end }}
