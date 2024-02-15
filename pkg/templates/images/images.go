63fce070672000e473e5fde6a45177ffd8b6cca8
Artiom Diomin <artiom@kubermatic.com>
2024-02-15 15:40:30 +0200

    Update OSM to v1.3.4
    Signed-off-by: Artiom Diomin <artiom@kubermatic.com>

--- pkg/templates/images/images.go
+++ pkg/templates/images/images.go
@@ -220,7 +220,7 @@ func baseResources() map[Resource]map[string]string {
 		Flannel:                {"*": "docker.io/flannel/flannel:v0.21.3"},
 		MachineController:      {"*": "quay.io/kubermatic/machine-controller:v1.57.4"},
 		MetricsServer:          {"*": "registry.k8s.io/metrics-server/metrics-server:v0.6.4"},
-		OperatingSystemManager: {"*": "quay.io/kubermatic/operating-system-manager:v1.3.3"},
+		OperatingSystemManager: {"*": "quay.io/kubermatic/operating-system-manager:v1.3.4"},
 	}
 }
 
