b96f8510e223bc032a2794f77b864e897a6f6943
Marko Mudrinić <mudrinic.mare@gmail.com>
2024-03-01 14:36:01 +0100

    Update machine-controller to v1.57.5 (#3057)
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- pkg/templates/images/images.go
+++ pkg/templates/images/images.go
@@ -218,7 +218,7 @@ func baseResources() map[Resource]map[string]string {
 		CalicoNode:             {"*": "quay.io/calico/node:v3.26.3"},
 		DNSNodeCache:           {"*": "registry.k8s.io/dns/k8s-dns-node-cache:1.22.23"},
 		Flannel:                {"*": "docker.io/flannel/flannel:v0.21.3"},
-		MachineController:      {"*": "quay.io/kubermatic/machine-controller:v1.57.4"},
+		MachineController:      {"*": "quay.io/kubermatic/machine-controller:v1.57.5"},
 		MetricsServer:          {"*": "registry.k8s.io/metrics-server/metrics-server:v0.6.4"},
 		OperatingSystemManager: {"*": "quay.io/kubermatic/operating-system-manager:v1.3.4"},
 	}
