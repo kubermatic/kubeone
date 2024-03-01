693959e419f691c6d8654103551d6ffcb380060d
Marko Mudrinić <mudrinic.mare@gmail.com>
2024-03-01 14:46:23 +0100

    Update Go to 1.21.6
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- test/e2e/helpers.go
+++ test/e2e/helpers.go
@@ -55,7 +55,7 @@ import (
 
 const (
 	labelControlPlaneNode = "node-role.kubernetes.io/control-plane"
-	prowImage             = "quay.io/kubermatic/build:go-1.21-node-18-9"
+	prowImage             = "quay.io/kubermatic/build:go-1.21-node-18-11"
 	k1CloneURI            = "ssh://git@github.com/kubermatic/kubeone.git"
 )
 
