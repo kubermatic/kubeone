48a5aa2b4fc750b03392f36e4aa6c0a768255fce
Karol Szwaj <karol.szwaj@gmail.com>
2024-03-11 13:49:37 +0100

    remove additional apt-get update
    Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>

--- pkg/scripts/os_debian.go
+++ pkg/scripts/os_debian.go
@@ -39,8 +39,6 @@ sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://pkgs.k8s.io/core:/stable:/{{ .KUBERNETES_MAJOR_MINOR }}/deb/Release.key | sudo gpg --dearmor --yes -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/{{ .KUBERNETES_MAJOR_MINOR }}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-sudo apt-get update
 {{- end }}
 
 sudo mkdir -p /etc/apt/apt.conf.d
