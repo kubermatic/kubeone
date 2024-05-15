583ae86ad767ae214d469b4f25e33d9679bbb283
Hubert Ströbitzer <hubert@kubermatic.com>
2024-05-15 13:23:20 +0300

    add node infos

--- pkg/dashboard/server.go
+++ pkg/dashboard/server.go
@@ -24,6 +24,9 @@ type dbData struct {
 
 type node struct {
 	Name    string
+	Status  string
+	Roles   string
+	Age     string
 	Version string
 }
 
@@ -84,19 +87,63 @@ func getNodes(s *state.State) ([]node, error) {
 	// Get node list
 	nodes := corev1.NodeList{}
 	nodeListOpts := dynclient.ListOptions{}
-
 	if err := s.DynamicClient.List(s.Context, &nodes, &nodeListOpts); err != nil {
 		return nil, fail.KubeClient(err, "listing nodes")
 	}
 
 	result := []node{}
-
 	for _, currNode := range nodes.Items {
+		// TODO is this safe
+		// TODO does this deliver really the last state?
+		// TODO nil checks
+		role, err := getNodeRole(&currNode)
+		if err != nil {
+			return nil, err
+		}
+
+		lastCondition := currNode.Status.Conditions[len(currNode.Status.Conditions)-1]
 		result = append(result, node{
 			Name:    currNode.Name,
+			Status:  string(lastCondition.Type),
+			Roles:   role,
+			Age:     lastCondition.LastHeartbeatTime.Format("2006-01-02 15:04:05"),
 			Version: currNode.Status.NodeInfo.KubeletVersion,
 		})
 	}
 
 	return result, nil
+}
+
+func getNodeRole(node *corev1.Node) (string, error) {
+	// TODO is this smart enough?
+	_, ok := node.ObjectMeta.Labels["node-role.kubernetes.io/control-plane"]
+	if ok {
+		return "control-plane", nil
+	}
+	return "<none>", nil
+}
+
+func getMachineDeployments(s *state.State) ([]machineDeployment, error) {
+	if s.DynamicClient == nil {
+		return nil, fail.NoKubeClient()
+	}
+
+	// // Get node list
+	// nodes := corev1.NodeList{}
+	// nodeListOpts := dynclient.ListOptions{}
+	// if err := s.DynamicClient.List(s.Context, &nodes, &nodeListOpts); err != nil {
+	// 	return nil, fail.KubeClient(err, "listing nodes")
+	// }
+
+	// result := []node{}
+	// for _, currNode := range nodes.Items {
+	// 	result = append(result, node{
+	// 		Name:    currNode.Name,
+	// 		Version: currNode.Status.NodeInfo.KubeletVersion,
+	// 	})
+	// }
+
+	// return result, nil
+
+	return nil, nil
 }
