34154ca740817d9582923bdfcee223ad8c46394a
Hubert Ströbitzer <hubert@kubermatic.com>
2024-05-22 11:51:56 +0200

    fix linting issues

--- pkg/dashboard/server.go
+++ pkg/dashboard/server.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2019 The KubeOne Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package dashboard
 
 import (
@@ -127,13 +143,13 @@ func getDashboardData(state *state.State) (*dashboardData, error) {
 		return nil, err
 	}
 
-	ret := dashboardData{
+	result := dashboardData{
 		ControlPlaneNodes:  nodes.ControlPlaneNodes,
 		WorkerNodes:        nodes.WorkerNodes,
 		MachineDeployments: machineDeployments,
 	}
 
-	return &ret, nil
+	return &result, nil
 }
 
 func getNodes(s *state.State) (*nodesResult, error) {
