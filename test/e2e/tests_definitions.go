41fd1005eea514b22c7bcaff0ed3d33f8083fc49
Simon Bein <simontheleg@gmail.com>
2024-07-10 14:29:53 +0200

    update test template name

--- test/e2e/tests_definitions.go
+++ test/e2e/tests_definitions.go
@@ -982,7 +982,7 @@ var (
 				path:    "../../examples/terraform/vsphere",
 				varFile: "testdata/vsphere.tfvars",
 				vars: []string{
-					"template_name=kubeone-ubuntu-22.04",
+					"template_name=kubeone-ubuntu-24.04",
 					"worker_os=ubuntu",
 					"ssh_username=ubuntu",
 				},
@@ -1006,7 +1006,7 @@ var (
 				path:    "../../../kubeone-stable/examples/terraform/vsphere",
 				varFile: "testdata/vsphere.tfvars",
 				vars: []string{
-					"template_name=kubeone-ubuntu-22.04",
+					"template_name=kubeone-ubuntu-24.04",
 					"worker_os=ubuntu",
 					"ssh_username=ubuntu",
 				},
