5165241a7fe58d09d0b165b6eedf02282957c697
Mohamed Rafraf <mohamedrafraf@insat.u-carthage.tn>
2024-08-15 09:38:41 +0100

    delete pid limit from v1beta2

--- pkg/apis/kubeone/v1beta2/zz_generated.deepcopy.go
+++ pkg/apis/kubeone/v1beta2/zz_generated.deepcopy.go
@@ -1034,11 +1034,6 @@ func (in *KubeletConfig) DeepCopyInto(out *KubeletConfig) {
 		*out = new(int32)
 		**out = **in
 	}
-	if in.PodPidsLimit != nil {
-		in, out := &in.PodPidsLimit, &out.PodPidsLimit
-		*out = new(int64)
-		**out = **in
-	}
 	return
 }
 
