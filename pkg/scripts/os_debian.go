443e5674c7bd39920c22a20c13397ee939e0037c
Mohamed Rafraf <mohamedrafraf@insat.u-carthage.tn>
2025-01-08 14:45:32 +0100

    fix installing old kubernetes versions

--- pkg/scripts/os_debian.go
+++ pkg/scripts/os_debian.go
@@ -72,10 +72,11 @@ sudo systemctl enable --now iscsid
 {{- if .CONFIGURE_REPOSITORIES }}
 sudo install -m 0755 -d /etc/apt/keyrings
 
-curl -fsSL https://pkgs.k8s.io/core:/stable:/{{ .KUBERNETES_MAJOR_MINOR }}/deb/Release.key | sudo gpg --dearmor --yes -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+LATEST_STABLE=$(curl -s https://cdn.dl.k8s.io/release/stable.txt | sed 's/\.[0-9]*$//')
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/${LATEST_STABLE}/deb/Release.key | sudo gpg --dearmor --yes -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/{{ .KUBERNETES_MAJOR_MINOR }}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
 sudo apt-get update
 {{- end }}
 
