fb3072103f86a7a3a889ea1c6d915bf7700670ed
Mohamed Rafraf <81432497+mohamed-rafraf@users.noreply.github.com>
2025-01-30 18:33:29 +0100

    labeling control-plane before (#3544)

--- pkg/tasks/tasks.go
+++ pkg/tasks/tasks.go
@@ -277,6 +277,11 @@ func WithResources(t Tasks) Tasks {
 				Predicate:   func(s *state.State) bool { return s.Cluster.CABundle != "" },
 			},
 			{
+				Fn:          labelNodes,
+				Operation:   "labeling control-plane nodes",
+				Description: "labeling control-plane nodes",
+			},
+			{
 				Fn:          addons.Ensure,
 				Operation:   "applying addons",
 				Description: "ensure embedded addons",
@@ -310,8 +315,9 @@ func WithResources(t Tasks) Tasks {
 				Operation: "joining static worker nodes to the cluster",
 			},
 			{
-				Fn:        labelNodes,
-				Operation: "labeling nodes",
+				Fn:          labelNodes,
+				Operation:   "labeling nodes",
+				Description: "labeling nodes",
 			},
 			{
 				Fn:        fixFilePermissions,
