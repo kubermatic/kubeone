0fbb19429cabf36ef2c069805cd7e84b6b030253
archups <archupsawant@gmail.com>
2025-09-15 15:27:31 +0530

    Add support for k8s version 1.34
    Signed-off-by: archups <archupsawant@gmail.com>

--- pkg/kubeflags/data.go
+++ pkg/kubeflags/data.go
@@ -33,6 +33,8 @@ var defaultAdmissionControllers = []string{
 	"ClusterTrustBundleAttest",
 	"CertificateSubjectRestriction",
 	"DefaultIngressClass",
+	"PodTopologyLabels",
+	"MutatingAdmissionPolicy",
 	"MutatingAdmissionWebhook",
 	"ValidatingAdmissionPolicy",
 	"ValidatingAdmissionWebhook",
