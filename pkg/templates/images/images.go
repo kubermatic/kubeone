2a0e348fd409d902fb935c223dfe6e04012052be
archups <archupsawant@gmail.com>
2025-09-15 15:37:03 +0530

    [release/v1.11] Bump MC to v1.62.1 and OSM to v1.7.6
    Signed-off-by: archups <archupsawant@gmail.com>

--- pkg/templates/images/images.go
+++ pkg/templates/images/images.go
@@ -223,9 +223,9 @@ func baseResources() map[Resource]map[string]string {
 		CalicoNode:             {"*": "quay.io/calico/node:v3.30.2"},
 		DNSNodeCache:           {"*": "registry.k8s.io/dns/k8s-dns-node-cache:1.25.0"},
 		Flannel:                {"*": "docker.io/flannel/flannel:v0.24.4"},
-		MachineController:      {"*": "quay.io/kubermatic/machine-controller:v1.62.0"},
+		MachineController:      {"*": "quay.io/kubermatic/machine-controller:v1.62.1"},
 		MetricsServer:          {"*": "registry.k8s.io/metrics-server/metrics-server:v0.7.2"},
-		OperatingSystemManager: {"*": "quay.io/kubermatic/operating-system-manager:v1.7.4"},
+		OperatingSystemManager: {"*": "quay.io/kubermatic/operating-system-manager:v1.7.6"},
 	}
 }
 
