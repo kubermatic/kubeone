ce730c6ed19ad7aa27b0bae6a44776bdb3dbc34b
Piotr Popieluch <piotr1212@gmail.com>
2025-12-15 20:09:24 +0100

    fix: set kubernetes-apt-keyring.gpg file permissions explicitly (#3940)
    Using a system with a tighter umask (027) reading the gpg keys fails.
    For unknown to me reasons apt needs the keyring to be others readable
    even when running as root. This is on Ubuntu 24.
    
    ```
    root@node:~# ls -l /etc/apt/keyrings/kubernetes-apt-keyring.gpg
    -rw-r----- 1 root root 1200 Dec 15 15:50 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
    ```
    
    ```
    root@node:~# apt update
    Hit:1 http://security.ubuntu.com/ubuntu noble-security InRelease
    Hit:2 https://prod-cdn.packages.k8s.io/repositories/isv:/kubernetes:/core:/stable:/v1.33/deb  InRelease
    Err:2 https://prod-cdn.packages.k8s.io/repositories/isv:/kubernetes:/core:/stable:/v1.33/deb  InRelease
      The following signatures couldn't be verified because the public key is not available: NO_PUBKEY 234654DA9A296436
    Get:3 https://pkgs.tailscale.com/stable/ubuntu noble InRelease
    Hit:4 http://gn2b.clouds.archive.ubuntu.com/ubuntu noble InRelease
    Hit:5 http://gn2b.clouds.archive.ubuntu.com/ubuntu noble-updates InRelease
    Hit:6 http://gn2b.clouds.archive.ubuntu.com/ubuntu noble-backports InRelease
    Fetched 6581 B in 2s (4151 B/s)
    Reading package lists... Done
    Building dependency tree... Done
    Reading state information... Done
    1 package can be upgraded. Run 'apt list --upgradable' to see it.
    W: An error occurred during the signature verification. The repository is not updated and the previous index files will be used. GPG error: https://prod-cdn.packages.k8s.io/repositories/isv:/kubernetes:/core:/stable:/v1.33/deb  InRelease: The following signatures couldn't be verified because the public key is not available: NO_PUBKEY 234654DA9A296436
    W: Failed to fetch https://pkgs.k8s.io/core:/stable:/v1.33/deb/InRelease  The following signatures couldn't be verified because the public key is not available: NO_PUBKEY 234654DA9A296436
    W: Some index files failed to download. They have been ignored, or old ones used instead.
    ```
    
    Setting the 0644 perms:
    ```
    root@sandbox-controlplane-node-gn2b:~# chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
    ```
    
    ```
    root@sandbox-controlplane-node-gn2b:~# apt update
    Hit:1 http://security.ubuntu.com/ubuntu noble-security InRelease
    Hit:2 https://prod-cdn.packages.k8s.io/repositories/isv:/kubernetes:/core:/stable:/v1.33/deb  InRelease
    Get:3 https://pkgs.tailscale.com/stable/ubuntu noble InRelease
    Hit:4 http://gn2b.clouds.archive.ubuntu.com/ubuntu noble InRelease
    Hit:5 http://gn2b.clouds.archive.ubuntu.com/ubuntu noble-updates InRelease
    Hit:6 http://gn2b.clouds.archive.ubuntu.com/ubuntu noble-backports InRelease
    Fetched 6581 B in 1s (5122 B/s)
    Reading package lists... Done
    Building dependency tree... Done
    Reading state information... Done
    1 package can be upgraded. Run 'apt list --upgradable' to see it.
    ```
    
    ```
    root@sandbox-controlplane-node-gn2b:~# id
    uid=0(root) gid=0(root) groups=0(root)
    ```
    
    Signed-off-by: Piotr Popieluch <piotr@pleio.nl>
    Co-authored-by: Piotr Popieluch <piotr@pleio.nl>

--- pkg/scripts/os_debian.go
+++ pkg/scripts/os_debian.go
@@ -71,6 +71,7 @@ fi
 sudo install -m 0755 -d /etc/apt/keyrings
 LATEST_STABLE=$(curl -sL https://dl.k8s.io/release/stable.txt | sed 's/\.[0-9]*$//')
 curl -fsSL https://pkgs.k8s.io/core:/stable:/${LATEST_STABLE}/deb/Release.key | sudo gpg --dearmor --yes -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo chmod 0644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/{{ .KUBERNETES_MAJOR_MINOR }}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update
