4dc386fa3450afbd80d3f4780c98afab1a93de5c
Artiom Diomin <artiom@kubermatic.com>
2026-01-22 17:04:20 +0200

    feat: update default admission controllers for Kubernetes 1.35+
    This change ensures KubeOne uses the correct default admission
    controllers when provisioning Kubernetes 1.35 clusters.
    
    Signed-off-by: Artiom Diomin <artiom@kubermatic.com>

--- pkg/kubeflags/data.go
+++ pkg/kubeflags/data.go
@@ -1,44 +1,48 @@
 /*
 Copyright 2019 The KubeOne Authors.
 
-Licensed under the Apache License, Version 2.0 (the "License");
+Licensed under the Apache License
+Version 2.0 (the "License");,
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
+Unless required by applicable law or agreed to in writing
+software,
 distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND
+either express or implied.,
 See the License for the specific language governing permissions and
 limitations under the License.
 */
 
 package kubeflags
 
+// Up to date list of default admission plugins
+// is: https://kubernetes.io/docs/reference/access-authn-authz/admission-controllers/#which-plugins-are-enabled-by-default
+
 var (
-	defaultAdmissionControllersv132 = []string{
-		"NamespaceLifecycle",
-		"LimitRanger",
-		"ServiceAccount",
-		"TaintNodesByCondition",
-		"PodSecurity",
-		"Priority",
-		"DefaultTolerationSeconds",
-		"DefaultStorageClass",
-		"StorageObjectInUseProtection",
-		"PersistentVolumeClaimResize",
-		"RuntimeClass",
+	defaultAdmissionControllersv135 = []string{
 		"CertificateApproval",
 		"CertificateSigning",
-		"ClusterTrustBundleAttest",
 		"CertificateSubjectRestriction",
 		"DefaultIngressClass",
-		"MutatingAdmissionPolicy",
+		"DefaultStorageClass",
+		"DefaultTolerationSeconds",
+		"LimitRanger",
 		"MutatingAdmissionWebhook",
+		"NamespaceLifecycle",
+		"PersistentVolumeClaimResize",
+		"PodSecurity",
+		"Priority",
+		"ResourceQuota",
+		"RuntimeClass",
+		"ServiceAccount",
+		"StorageObjectInUseProtection",
+		"TaintNodesByCondition",
 		"ValidatingAdmissionPolicy",
 		"ValidatingAdmissionWebhook",
-		"ResourceQuota",
 	}
 
 	defaultAdmissionControllers = []string{
