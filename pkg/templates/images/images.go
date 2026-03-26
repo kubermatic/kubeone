57c85af2bf67b67d4d587931c19906ca1680bf09
Artiom Diomin <artiom@kubermatic.com>
2026-03-26 12:04:51 +0200

    chore: bump machine-controller to v1.65.0
    Signed-off-by: Artiom Diomin <artiom@kubermatic.com>

--- pkg/templates/images/images.go
+++ pkg/templates/images/images.go
@@ -236,7 +236,7 @@ func baseResources() map[Resource]map[string]string {
 		CalicoNode:             {"*": "quay.io/calico/node:v3.31.2"},
 		DNSNodeCache:           {"*": "registry.k8s.io/dns/k8s-dns-node-cache:1.26.4"},
 		Flannel:                {"*": "docker.io/flannel/flannel:v0.24.4"},
-		MachineController:      {"*": "quay.io/kubermatic/machine-controller:01ad12aa435b2644bb76081ea7ebc00c451cf0dc"},
+		MachineController:      {"*": "quay.io/kubermatic/machine-controller:v1.65.0"},
 		MetricsServer:          {"*": "registry.k8s.io/metrics-server/metrics-server:v0.8.0"},
 		OperatingSystemManager: {"*": "quay.io/kubermatic/operating-system-manager:v1.10.3"},
 	}
