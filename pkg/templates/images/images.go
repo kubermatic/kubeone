eebf254f186765ce61710b6cc04ee7a1de518dda
Artiom Diomin <artiom@kubermatic.com>
2026-03-26 12:09:30 +0200

    chore: bump metrics-server to v0.8.1
    Signed-off-by: Artiom Diomin <artiom@kubermatic.com>

--- pkg/templates/images/images.go
+++ pkg/templates/images/images.go
@@ -237,7 +237,7 @@ func baseResources() map[Resource]map[string]string {
 		DNSNodeCache:           {"*": "registry.k8s.io/dns/k8s-dns-node-cache:1.26.4"},
 		Flannel:                {"*": "docker.io/flannel/flannel:v0.24.4"},
 		MachineController:      {"*": "quay.io/kubermatic/machine-controller:v1.65.0"},
-		MetricsServer:          {"*": "registry.k8s.io/metrics-server/metrics-server:v0.8.0"},
+		MetricsServer:          {"*": "registry.k8s.io/metrics-server/metrics-server:v0.8.1"},
 		OperatingSystemManager: {"*": "quay.io/kubermatic/operating-system-manager:v1.10.3"},
 	}
 }
