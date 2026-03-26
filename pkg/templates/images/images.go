f18dbdb725eb4b28cabbc08d9a8653fb73be988e
Artiom Diomin <artiom@kubermatic.com>
2026-03-26 13:15:11 +0200

    chore: bump canal CNI to v3.31.4
    Signed-off-by: Artiom Diomin <artiom@kubermatic.com>

--- pkg/templates/images/images.go
+++ pkg/templates/images/images.go
@@ -231,9 +231,9 @@ func FindResource(name string) (Resource, error) {
 
 func baseResources() map[Resource]map[string]string {
 	return map[Resource]map[string]string{
-		CalicoCNI:              {"*": "quay.io/calico/cni:v3.31.2"},
-		CalicoController:       {"*": "quay.io/calico/kube-controllers:v3.31.2"},
-		CalicoNode:             {"*": "quay.io/calico/node:v3.31.2"},
+		CalicoCNI:              {"*": "quay.io/calico/cni:v3.31.4"},
+		CalicoController:       {"*": "quay.io/calico/kube-controllers:v3.31.4"},
+		CalicoNode:             {"*": "quay.io/calico/node:v3.31.4"},
 		DNSNodeCache:           {"*": "registry.k8s.io/dns/k8s-dns-node-cache:1.26.7"},
 		Flannel:                {"*": "docker.io/flannel/flannel:v0.24.4"},
 		MachineController:      {"*": "quay.io/kubermatic/machine-controller:v1.65.0"},
