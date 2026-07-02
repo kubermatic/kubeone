e28ddecbcc3712c5c83c39b26d238c496faab4d9
Artiom Diomin <artiom@kubermatic.com>
2026-07-02 11:26:12 +0300

    Update container images for MachineController and OperatingSystemManager
    Signed-off-by: Artiom Diomin <artiom@kubermatic.com>

--- pkg/templates/images/images.go
+++ pkg/templates/images/images.go
@@ -236,9 +236,9 @@ func baseResources() map[Resource]map[string]string {
 		CalicoNode:             {"*": "quay.io/calico/node:v3.32.0"},
 		DNSNodeCache:           {"*": "registry.k8s.io/dns/k8s-dns-node-cache:1.26.7"},
 		Flannel:                {"*": "docker.io/flannel/flannel:v0.24.4"},
-		MachineController:      {"*": "quay.io/kubermatic/machine-controller:090279e10f6557926c29fce95405df4eaea44241"},
+		MachineController:      {"*": "quay.io/kubermatic/machine-controller:bd64ac8c0b3f96206479c74b6815a94d2ddcd6bd"},
 		MetricsServer:          {"*": "registry.k8s.io/metrics-server/metrics-server:v0.8.1"},
-		OperatingSystemManager: {"*": "quay.io/kubermatic/operating-system-manager:0d531b49eca2a91b0e6c61609a32468668a615bb"},
+		OperatingSystemManager: {"*": "quay.io/kubermatic/operating-system-manager:42ade2ddf5c9bd3a6966b890d53926e4fc1752cf"},
 	}
 }
 
