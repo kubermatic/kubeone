db1d71fe99caf857d305141e9738595b959c6f2d
Artiom Diomin <artiom@kubermatic.com>
2026-09-07 14:25:28 +0300

    Add Azure managed control plane Terraform configuration files
    Signed-off-by: Artiom Diomin <artiom@kubermatic.com>

---
+++ examples/terraform/azure-managed-control-plane/variables.tf
@@ -0,0 +1,33 @@
+/*
+Copyright 2019 The KubeOne Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+variable "cluster_name" {
+  description = "Name of the cluster"
+  type        = string
+
+  validation {
+    condition     = can(regex("^[a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$", var.cluster_name))
+    error_message = "Value of cluster_name should be lowercase and can only contain alphanumeric characters and hyphens(-)."
+  }
+}
+
+# Provider specific settings
+
+variable "location" {
+  description = "Azure datacenter to use"
+  default     = "westeurope"
+  type        = string
+}
